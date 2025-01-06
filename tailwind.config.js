d1fe111b0dc08f2d952a6dd178928584e9fd792d
“chae-dahee” <cdh010126r@gmail.com>
2025-01-06 20:20:35 +0900

    chore: tailwindcss setting

---
+++ tailwind.config.js
@@ -0,0 +1,96 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    fontFamily: {
+      pretendard: ["var(--font-pretendard)", "sans-serif"],
+    },
+    fontSize: {
+      title: [
+        "2.25rem",
+        {
+          letterSpacing: "2.5%",
+          fontWeight: "600",
+        },
+      ],
+      pageTitle: [
+        "1.5rem",
+        {
+          letterSpacing: "2.5%",
+          fontWeight: "600",
+        },
+      ],
+      subTitle: [
+        "1.25rem",
+        {
+          letterSpacing: "2.5%",
+          fontWeight: "500",
+        },
+      ],
+      base: [
+        "1rem",
+        {
+          letterSpacing: "2.5%",
+          fontWeight: "400",
+        },
+      ],
+      small: [
+        "0.75rem",
+        {
+          letterSpacing: "2.5%",
+          fontWeight: "400",
+        },
+      ],
+    },
+    extend: {
+      colors: {
+        primary: {
+          5: "var(--purple-5)",
+          10: "var(--purple-10)",
+          20: "var(--purple-20)",
+          30: "var(--purple-30)",
+          40: "var(--purple-40)",
+          base: "var(--purple-50)",
+          60: "var(--purple-60)",
+          70: "var(--purple-70)",
+          80: "var(--purple-80)",
+          90: "var(--purple-90)",
+          red: "var(--primary-red)",
+          yellow: "var(--primary-yellow)",
+          green: "var(--primary-green)",
+          blue: "var(--primary-blue)",
+        },
+        neutral: {
+          bg: {
+            5: "var(--neutral-5)",
+            10: "var(--neutral-10)",
+          },
+          disabled: "var(--neutral-20)",
+          border: {
+            30: "var(--neutral-30)",
+            40: "var(--neutral-40)",
+            50: "var(--neutral-50)",
+          },
+          base: "var(--neutral-60)",
+          70: "var(--neutral-70)",
+          80: "var(--neutral-80)",
+          title: "var(--neutral-90)",
+        },
+      },
+      boxShadow: {
+        base: "1px 4px 10px rgba(0, 0, 0, 0.2)",
+      },
+      keyframes: {
+        "bottom-sheet-up": {
+          "0%": { transform: "translateY(100dvh)", opacity: "0" },
+          "100%": { transform: "translateY(0)", opacity: "1" },
+        },
+        "bottom-sheet-down": {
+          "0%": { transform: "translateY(0)", opacity: "1" },
+          "100%": { transform: "translateY(100dvh)", opacity: "0" },
+        },
+      },
+    },
+  },
+  plugins: [],
+};
