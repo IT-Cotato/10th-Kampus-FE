be4fc51809239e99774b9378c89df8cdfeae4250
seoyeon5117 <seoyun020207@gmail.com>
2025-01-20 21:59:52 +0900

    fix: JoinInButton disabled 속성 추가

--- src/components/common/JoinInButton.jsx
+++ src/components/common/JoinInButton.jsx
@@ -1,6 +1,6 @@
 export const JoinInButton = (props) => {
     return (
-      <button onClick={props.onClick} className={`w-[100%] h-[59px] ${props.disabled ? "bg-neutral-disabled cursor-default" : "bg-primary-base cursor-pointer"} text-white rounded-[10px]`}>
+      <button onClick={props.onClick} disabled={props.disabled} className={`w-[100%] h-[59px] ${props.disabled ? "bg-neutral-disabled cursor-default" : "bg-primary-base cursor-pointer"} text-white rounded-[10px]`}>
         {props.children}
       </button>
     );
