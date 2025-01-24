51a188e5d730ef1cf247a3910606c69599b6cf8f
“chae-dahee” <cdh010126r@gmail.com>
2025-01-24 21:24:58 +0900

    feat: 상단 title 헤더 컴포넌트, animation 추가

---
+++ src/components/common/titleHeader.jsx
@@ -0,0 +1,20 @@
+import arrowLeft from '@/assets/imgs/navIcon/arrowLeft.svg';
+import { path } from '@/routes/path';
+import { useNavigate } from 'react-router-dom';
+
+export const TitleHeader = ({ text }) => {
+  const navigate = useNavigate();
+
+  return (
+    <div className="h-16 flex flex-row p-[0.625rem] items-center justify-between">
+      <img
+        src={arrowLeft}
+        alt="arrowLeft"
+        onClick={() => navigate(path.mypage.base)}
+        className="cursor-pointer w-[1.25rem] h-[1.25rem]"
+      />
+      <span className="mx-auto text-pageTitle text-neutral-title">{text}</span>
+      <div className="w-[1.25rem] h-[1.25rem]" />
+    </div>
+  );
+};
