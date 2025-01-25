e5db6a23a480792fc1e78b3823ebe32c8d40d782
“chae-dahee” <cdh010126r@gmail.com>
2025-01-25 19:06:22 +0900

    fix: 사용자 정보 없을 시 navbar 렌더 차단

--- src/components/layout/layout.jsx
+++ src/components/layout/layout.jsx
@@ -1,6 +1,11 @@
 import { Navbar } from '@/components/layout/navbar';
+import { useLocation } from 'react-router-dom';
 
 export const Layout = ({ children }) => {
+  const location = useLocation();
+  const renderNav =
+    !location.pathname.includes('login') && !location.pathname.includes('sign');
+
   return (
     <div className="layout">
       <div
@@ -8,7 +13,8 @@ export const Layout = ({ children }) => {
         className="container relative flex flex-col justify-between"
       >
         {children}
-        <Navbar />
+        {/* 사용자 정보 존재 시 render 개선 */}
+        {renderNav && <Navbar />}
       </div>
     </div>
   );
