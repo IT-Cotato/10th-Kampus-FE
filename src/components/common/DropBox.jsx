9ca670e49ca9e91aba3738aa45fe4cff8190b89f
Ki-Tak <lee08tain@gmail.com>
2025-01-27 16:44:44 +0900

    일반 게시글 리스트 레이아웃 구현

--- src/components/common/DropBox.jsx
+++ src/components/common/DropBox.jsx
@@ -4,7 +4,7 @@ export const DropBox = ({ state, content, dropList, toggle, select, selected }) 
     return (
         <div
             onClick={toggle}
-            className={cn('relative flex justify-center items-center gap-3 px-[1.125rem] rounded-full py-[0.375rem] border', { 'border-primary-30': selected !== 'All' || state, ' shadow-md': selected === 'All' && !state })}>
+            className={cn('cursor-pointer relative flex justify-center items-center gap-3 px-[1.125rem] rounded-full py-[0.375rem] border', { 'border-primary-30': selected !== 'All' || state, ' shadow-md': selected === 'All' && !state })}>
             <p className={cn('text-base font-normal', { 'text-neutral-title': selected !== 'All' || state, 'text-[#8E8E8E]': selected === 'All' && !state })}>{selected !== 'All' ? selected : content}</p>
             <Dropdown className={cn('w-3 h-3', { 'text-neutral-title': selected !== 'All' || state, 'text-[#8E8E8E] rotate-180': selected === 'All' && !state })} />
             {state &&
@@ -13,7 +13,7 @@ export const DropBox = ({ state, content, dropList, toggle, select, selected }) 
                         <p
                             key={category}
                             onClick={() => select(category)}
-                            className="text-base text-neutral-title py-1 w-full">
+                            className="text-base text-neutral-title py-1 w-full cursor-pointer">
                             {category}</p>
                     )}
                 </div>
