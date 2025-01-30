7c049afb5c0134d025db5109e28d1a6814c946af
seoyeon5117 <seoyun020207@gmail.com>
2025-01-30 23:07:47 +0900

    feat: 팝업 스크롤 막기, 화면 중앙 위치하게 수정

--- src/components/common/popup.jsx
+++ src/components/common/popup.jsx
@@ -1,9 +1,25 @@
+import { useEffect } from 'react';
+
 const Popup = (props) => {
+  useEffect(() => {
+    // 스크롤 막기
+    document.body.style.overflow = 'hidden';
+
+    return () => {
+      // 컴포넌트가 언마운트될 때 원래 상태로 복구
+      document.body.style.overflow = 'auto';
+    };
+  }, []);
+
   return (
-    <div className="max-w-lg mx-auto min-h-dvh w-full h-full overflow-x-hidden overflow-y-auto bg-[rgba(11,11,11,0.6)] absolute flex z-[100] justify-center items-center align-middle">
+    <div className="max-w-lg mx-auto min-h-dvh w-full h-full overflow-x-hidden overflow-y-auto bg-[rgba(11,11,11,0.6)] fixed flex z-[100] justify-center items-center align-middle">
       <dialog className="z-[200] flex flex-col items-center justify-center bg-white border py-[1.875rem] gap-[1.875rem] mx-4 rounded-[.625rem]">
-        <div className="flex items-center justify-center w-full text-center align-middle text-subTitle">{props.title}</div>
-        <div className="flex items-center justify-center w-full px-10 text-base text-center align-middle text-neutral-base">{props.text}</div>
+        <div className="flex items-center justify-center w-full text-center align-middle text-subTitle">
+          {props.title}
+        </div>
+        <div className="flex items-center justify-center w-full px-10 text-base text-center align-middle text-neutral-base">
+          {props.text}
+        </div>
         <div className="flex flex-row gap-4">
           <button
             type="submit"
