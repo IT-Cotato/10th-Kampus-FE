c0c0e5fa1a3bb376c09dbb150254bf5c441ebe93
seoyeon5117 <seoyun020207@gmail.com>
2025-01-30 23:00:52 +0900

    feat: 탈퇴 페이지 popup 컴포넌트 적용

--- src/pages/my/etc/deleteAccount.jsx
+++ src/pages/my/etc/deleteAccount.jsx
@@ -1,11 +1,15 @@
 import InputRadio from '@/components/common/inputRadio';
 import MainButton from '@/components/common/MainButton';
+import Popup from '@/components/common/popup';
 import { TitleHeader } from '@/components/common/titleHeader';
+import { path } from '@/routes/path';
 import { useState } from 'react';
+import { createPortal } from 'react-dom';
+import { useNavigate } from 'react-router-dom';
 
 export const DeleteAccount = () => {
   const Reasons = [
-    { id: 0, text: 'Studying abroad in Korea is over'},
+    { id: 0, text: 'Studying abroad in Korea is over' },
     { id: 1, text: 'Too much advertising' },
     { id: 2, text: 'Too many errors' },
     { id: 3, text: 'Hard to use' },
@@ -14,14 +18,20 @@ export const DeleteAccount = () => {
 
   const [selected, setSelected] = useState('');
   const [additionalText, setAdditionalText] = useState('');
+  const [showModal, setShowModal] = useState(false);
 
-  const handleDeleteAccountClick = () => {
+  const navigate = useNavigate();
+
+  const handleClickLeave = () => {
     // 추후 백엔드와 연동
     if (selected === 'Other') {
       console.log(selected + ': ' + additionalText);
     } else {
       console.log(selected);
     }
+
+    // 계정 삭제 후 로그인(회원가입) 페이지로 이동
+    navigate(path.login);
   };
 
   return (
@@ -54,10 +64,22 @@ export const DeleteAccount = () => {
               selected !== 'Other'
             )
           }
-          onClick={handleDeleteAccountClick}
+          onClick={() => setShowModal(true)}
         >
           Delete Account
         </MainButton>
+        {showModal &&
+          createPortal(
+            <Popup
+              title="Are you sure you're leaving Kampus?"
+              text="When you delete your account, all personal information and your articles will be deleted."
+              onClickLeft={handleClickLeave}
+              leftButton="Leave now"
+              onClickRight={() => setShowModal(false)}
+              rightButton="I don't want to leave"
+            />,
+            document.getElementById('modal-root'),
+          )}
       </div>
     </div>
   );
