e128b8eb53cc3f55e086e9e34cf43f1797870bae
seoyeon5117 <seoyun020207@gmail.com>
2025-02-02 04:04:54 +0900

    fix: 'process.env': {} 삭제

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import svgr from 'vite-plugin-svgr';
-import path from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), svgr()],
-  define: {
-    'process.env': {}
-  },
-  server: {
-    host: '0.0.0.0', // 모든 네트워크 인터페이스에서 접근 가능
-    port: 3000,
-  },
   resolve: {
     alias: [{ find: '@', replacement: '/src' }],
   },
