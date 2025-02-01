b5f0f28fd19166869c65b8206f577fcc736abd0a
Ki-Tak <lee08tain@gmail.com>
2025-02-01 18:42:36 +0900

    fix : 애니메이션 모달 포탈로 변경

--- src/components/common/StateChangeAnimate.jsx
+++ src/components/common/StateChangeAnimate.jsx
@@ -1,5 +1,11 @@
+import ReactDOM from 'react-dom';
+
 export const StateChangeAnimate = ({ state, changeToTrueText, changeToFalseText }) => {
-    return (
+    // 포털을 사용해 렌더링할 대상 DOM 요소 선택
+    const portalRoot = document.getElementById('portal-root');
+
+    // 포털이 적용된 컴포넌트 반환
+    return ReactDOM.createPortal(
         <>
             <div className="absolute inset-0 z-40 flex justify-center items-center bg-neutral-700  
         animate-fadeInOutModal"
@@ -7,8 +13,11 @@ export const StateChangeAnimate = ({ state, changeToTrueText, changeToFalseText 
             <div className="absolute z-50 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 
         rounded-xl px-12 py-4 bg-white animate-fadeInOutText"
             >
-                <p className="truncate text-[#525252]">{state ? `${changeToFalseText}` : `${changeToTrueText}`}</p>
+                <p className="truncate text-[#525252]">
+                    {state ? `${changeToFalseText}` : `${changeToTrueText}`}
+                </p>
             </div>
-        </>
-    )
-}
+        </>,
+        portalRoot // 포털 대상 지정
+    );
+};
