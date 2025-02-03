42b61882b73cbced3ca49f09a380bf9876995b27
seoyeon5117 <seoyun020207@gmail.com>
2025-02-04 04:13:08 +0900

    feat: 게시글 작성 페이지 구현

--- src/pages/board/write.jsx
+++ src/pages/board/write.jsx
@@ -1,6 +1,73 @@
+import X from '@/assets/imgs/x.svg';
+import { WriteTitle } from '@/components/board/write/WriteTitle';
+import { WriteContent } from '@/components/board/write/WriteContent';
+import { UploadPics } from '@/components/board/write/UploadPics';
+import { MainButton } from '@/components/common/MainButton';
+import { useState } from 'react';
+import { useNavigate, useParams } from 'react-router-dom';
+import { SelectCategory } from '@/components/board/write/SelectCategory';
+
 export const Write = () => {
+  const { boardTitle } = useParams();
+  const navigate = useNavigate();
+  const [title, setTitle] = useState('');
+  const [selectedCategory, setSelectedCategory] = useState('');
+  const [content, setContent] = useState('');
+  const [translatedContent, setTranslatedContent] = useState('');
+  const [uploadedFiles, setUploadedFiles] = useState([]);
+
+  const disabled = !title || !content;
+
+  const handleUpload = () => {
+    // 백 연동
+    console.log(title);
+    if (selectedCategory) {
+      console.log(selectedCategory);
+    }
+    console.log(content);
+    if (uploadedFiles) {
+      console.log(uploadedFiles);
+    }
+  };
+
   return (
     <div className="flex flex-col w-full h-full">
+      <div className="grid items-center w-full grid-cols-3 px-4 pt-4 pb-3">
+        <button type="button">
+          <img
+            src={X}
+            className="w-6 h-6 p-1 text-neutral-title"
+            onClick={() => navigate('..')}
+          />
+        </button>
+        <span className="flex justify-center text-neutral-title text-pageTitle">
+          Write
+        </span>
+        <span className="flex items-center justify-end gap-2 text-neutral-border-50">
+          <button type="button">Save Draft</button>
+          <span>|</span>
+          <button type="button">00</button>
+        </span>
+      </div>
+      <div className="flex flex-col w-full h-full px-4 py-[1.25rem] gap-[2.5rem]">
+        <WriteTitle title={title} setTitle={setTitle} />
+        {(boardTitle === 'question' || boardTitle === 'information') && (
+          <SelectCategory
+            selectedCategory={selectedCategory}
+            setSelectedCategory={setSelectedCategory}
+          />
+        )}
+        <WriteContent
+          content={content}
+          setContent={setContent}
+          translatedContent={translatedContent}
+          setTranslatedContent={setTranslatedContent}
+        />
+        <UploadPics onChange={setUploadedFiles} />
+        <MainButton onClick={handleUpload} disabled={disabled}>
+          Upload
+        </MainButton>
+      </div>
     </div>
   );
 };
