50845a337da0baef0cfb0c03452ba132c6976ea2
“chae-dahee” <cdh010126r@gmail.com>
2025-02-06 17:20:03 +0900

    feat: chatlist 안읽은 메시지 표시

---
+++ src/components/chat/newMsgCnt.jsx
@@ -0,0 +1,7 @@
+export const NewMsgCnt = ({ cnt }) => {
+  return (
+    <div className="h-[1.25rem] min-w-[1.25rem] max-w-[2.5rem] rounded-[6.25rem] bg-primary-red px-2 text-small text-white">
+      {cnt}
+    </div>
+  );
+};
