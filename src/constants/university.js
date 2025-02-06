f35e5f9e5e93b3dab0abe2a92684d9b444cc2f41
seoyeon5117 <seoyun020207@gmail.com>
2025-02-06 19:54:43 +0900

    feat: 학교 검색 페이지 구현

---
+++ src/constants/university.js
@@ -0,0 +1,21 @@
+// 더미 데이터
+const University = [
+  { name: 'Hongik University', code: 'HIU' },
+  { name: 'Seoul National University', code: 'SNU' },
+  { name: 'KAIST', code: 'KAIST' },
+  { name: 'POSTECH', code: 'POSTECH' },
+  { name: 'GIST', code: 'GIST' },
+  { name: 'DGIST', code: 'DGIST' },
+  { name: 'UNIST', code: 'UNIST' },
+  { name: 'Korea University', code: 'KU' },
+  { name: 'Yonsei University', code: 'YU' },
+  { name: 'Hanyang University', code: 'HYU' },
+  { name: 'Sungkyunkwan University', code: 'SKKU' },
+  { name: 'Ewha Womans University', code: 'EWU' },
+  { name: 'Sogang University', code: 'SGU' },
+  { name: 'Chung-Ang University', code: 'CAU' },
+  { name: 'Kyungpook National University', code: 'KNU' },
+  { name: 'Kyung Hee University', code: 'KHU' },
+];
+
+export default University;
--- src/pages/auth/schoolSearch.jsx
+++ src/pages/auth/schoolSearch.jsx
@@ -1,7 +1,36 @@
+import University from '@/constants/university';
+import { path } from '@/routes/path';
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { MainButton } from '@/components/common/MainButton';
+import { SearchDropdownWithNoLabel } from '@/components/join/searchDropdownWithNoLabel';
+
 export const SchoolSearch = () => {
+  const UniversityList = University;
+  const [university, setUniversity] = useState('');
+  const [isUniversitySelected, setIsUniversitySelected] = useState(false);
+  const navigate = useNavigate();
+
   return (
-    <div className="flex flex-col w-full h-full px-4">
-        <div className="flex justify-center w-full text-pageTitle">What is your school name?</div>
+    <div className="flex flex-col w-full h-full px-4 py-[3.25rem] gap-[1.875rem]">
+      <div className='flex flex-col gap-[3.125rem]'>
+        <div className="flex justify-center w-full text-pageTitle gap-7">
+          What is your school name?
+        </div>
+        <SearchDropdownWithNoLabel
+          keyword={university}
+          name="Language"
+          placeholder="Search your school"
+          onChange={(value) => setUniversity(value)}
+          setIsSelected={setIsUniversitySelected}
+          selected={isUniversitySelected}
+          list={UniversityList}
+          warn="No school found. Kampus supports 397 universities in Korea."
+        />
+      </div>
+      <MainButton onClick={() => navigate(`../${path.signup.verify}`)} disabled={!isUniversitySelected}>
+        Next
+      </MainButton>
     </div>
   );
 };
