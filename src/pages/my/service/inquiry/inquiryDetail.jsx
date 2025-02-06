bd418539001cc69bfe051eb818c4c1e9ff87e0d6
seoyeon5117 <seoyun020207@gmail.com>
2025-02-06 15:55:34 +0900

    design: 답변 배경색 수정 및 필요없는 import 삭제

--- src/pages/my/service/inquiry/inquiryDetail.jsx
+++ src/pages/my/service/inquiry/inquiryDetail.jsx
@@ -1,5 +1,3 @@
-import { WriteContent } from '@/components/board/write/WriteContent';
-import { WriteTitle } from '@/components/board/write/WriteTitle';
 import { TitleHeader } from '@/components/common/titleHeader';
 import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
@@ -47,7 +45,7 @@ export const InquiryDetail = () => {
             <div className="w-fit border border-primary-20 px-3 py-1 rounded-[1.25rem] text-base text-neutral-base bg-primary-10">
               Answered
             </div>
-            <div className="box-border flex flex-row w-full px-[.875rem] py-[1.125rem] border border-neutral-border-30 rounded-lg gap-[.625rem]">
+            <div className="box-border flex flex-row w-full px-[.875rem] py-[1.125rem] border border-primary-20 bg-primary-5 rounded-lg gap-[.625rem]">
               <div className="w-full leading-none placeholder-neutral-border-50 h-[12.5rem]">
                 {answer}
               </div>
