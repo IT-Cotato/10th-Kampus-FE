82f25fa6ba50b362eb51867cd7d541b52a2f7b21
seoyeon5117 <seoyun020207@gmail.com>
2025-02-08 18:56:34 +0900

    feat: 차단 리스트 구현

---
+++ src/components/block/ChatBlockedList.jsx
@@ -0,0 +1,23 @@
+import { useNavigate } from 'react-router-dom';
+
+export const ChatBlockedList = ({ title, content, userId, onClick }) => {
+
+  return (
+    <div className="flex flex-row items-center justify-between w-full gap-3 py-5 border-b border-primary-20">
+      <div className="flex flex-col w-full truncate">
+        <span className="truncate text-subTitle text-neutral-title">
+          {title}
+        </span>
+        <span className="inline-block truncate text-neutral-base">
+          {content}
+        </span>
+      </div>
+      <button
+        className="inline-block rounded-[20px] border border-primary-20 px-3 py-1 text-base text-neutral-base"
+        onClick={() => onClick(userId)}
+      >
+        Unblock
+      </button>
+    </div>
+  );
+};
--- src/pages/my/etc/blockChat.jsx
+++ src/pages/my/etc/blockChat.jsx
@@ -1,7 +1,50 @@
+import { ChatBlockedList } from "@/components/block/ChatBlockedList";
+
 export const BlockChat = () => {
+  const list = [
+    {
+      id: 0,
+      title: 'Very very very very very very long title',
+      content: 'Lorem ipsum dolor sit amet consectetur. Tortor dictumst nam tortor diam semper lorem ut. Facilisis ultrices diam amet cursus. Malesuada sed feugiat ridiculus nunc ultrices pellentesque tellus et velit. Nisi id consectetur elementum malesuada scelerisque ut auctor lacus quis. Quam mattis magna ut nullam. Aliquam facilisis nulla convallis a varius eget velit mattis dui. Arcu curabitur faucibus montes donec. At elit quisque ipsum luctus nunc. Cursus vestibulum id fringilla egestas commodo.',
+    },
+    {
+      id: 1,
+      title: 'Article Title',
+      content: 'content',
+    },
+    {
+      id: 2,
+      title: 'Article Title',
+      content: 'contentcontentcontentcontentcontentcontentcontentcontentcontent',
+    },
+  ];
+
+  let count = 1;
+
+  const handleUnblock = (userId) => {
+    console.log(userId + ' unblocked');
+  };
+
   return (
     <div className="flex flex-col w-full h-full">
-      채팅 차단목록
-    </div>
+            {count === 0 ? (
+              // data.inquiry.length === 0
+              <div className='flex flex-col items-center justify-center w-full h-full gap-[.625rem]'>
+                <span></span>
+              </div>
+            ) : (
+              <div className='flex flex-col w-full'>
+                {list.map((item, index) => (
+                  <ChatBlockedList
+                    key={index}
+                    title={item.title}
+                    content={item.content}
+                    userId={item.id}
+                    onClick={handleUnblock}
+                  />
+                ))}
+              </div>
+            )}
+          </div>
   );
 };
