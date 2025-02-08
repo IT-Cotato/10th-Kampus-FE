b68af093f1d12a90da2bc5250e8a7add187f5876
seoyeon5117 <seoyun020207@gmail.com>
2025-02-08 16:26:46 +0900

    feat: 학교 검색 페이지 skip 버튼 추가, 다음 페이지로 state 전달

--- src/pages/auth/schoolSearch.jsx
+++ src/pages/auth/schoolSearch.jsx
@@ -4,6 +4,7 @@ import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { MainButton } from '@/components/common/MainButton';
 import { SearchDropdownWithNoLabel } from '@/components/join/searchDropdownWithNoLabel';
+import { SkipButton } from '@/components/join/skipButtonShadow';
 
 export const SchoolSearch = () => {
   const UniversityList = University;
@@ -12,25 +13,35 @@ export const SchoolSearch = () => {
   const navigate = useNavigate();
 
   return (
-    <div className="flex flex-col w-full h-full px-4 py-[3.25rem] gap-[1.875rem]">
-      <div className='flex flex-col gap-[3.125rem]'>
-        <div className="flex justify-center w-full text-pageTitle gap-7">
-          What is your school name?
-        </div>
-        <SearchDropdownWithNoLabel
-          keyword={university}
-          name="Language"
-          placeholder="Search your school"
-          onChange={(value) => setUniversity(value)}
-          setIsSelected={setIsUniversitySelected}
-          selected={isUniversitySelected}
-          list={UniversityList}
-          warn="No school found. Kampus supports 397 universities in Korea."
-        />
+    <div className="flex flex-col flex-1 gap-10 px-4 py-5">
+      <div className="flex justify-end">
+        <SkipButton navigateTo={`../../${path.home}`} />
       </div>
-      <MainButton onClick={() => navigate(`../${path.signup.verify}`)} disabled={!isUniversitySelected}>
-        Next
-      </MainButton>
+      <div className="flex flex-col gap-5">
+        <div className="flex flex-col gap-[3.125rem]">
+          <div className="flex justify-center w-full text-pageTitle gap-7">
+            What is your school name?
+          </div>
+          <SearchDropdownWithNoLabel
+            keyword={university}
+            name="Language"
+            placeholder="Search your school"
+            onChange={(value) => setUniversity(value)}
+            setIsSelected={setIsUniversitySelected}
+            selected={isUniversitySelected}
+            list={UniversityList}
+            warn="No school found. Kampus supports 397 universities in Korea."
+          />
+        </div>
+        <MainButton
+          onClick={() =>
+            navigate(`../${path.signup.verify}`, { state: university })
+          }
+          disabled={!isUniversitySelected}
+        >
+          Next
+        </MainButton>
+      </div>
     </div>
   );
 };
