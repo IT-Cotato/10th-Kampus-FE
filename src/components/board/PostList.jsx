f14dd5cdf76d0837e7430472cae361903e7d4ae7
seoyeon5117 <seoyun020207@gmail.com>
2025-02-10 04:56:32 +0900

    feat: postList 말줄임표 추가

--- src/components/board/PostList.jsx
+++ src/components/board/PostList.jsx
@@ -1,37 +1,43 @@
-import Like from '../../assets/imgs/like.svg?react'
-import Comment from '../../assets/imgs/comment.svg?react'
-import Translate from '../../assets/imgs/translate.svg?react'
+import Like from '../../assets/imgs/like.svg?react';
+import Comment from '../../assets/imgs/comment.svg?react';
+import Translate from '../../assets/imgs/translate.svg?react';
 export const PostList = ({ data, isActive }) => {
-    return (
-        <div className="flex flex-col gap-3 pt-4 pb-3">
-            {isActive &&   /** 인기 게시판 레이아웃 */
-                <div className='py-[0.3125rem] w-fit px-[0.625rem] rounded-md bg-primary-10
-                text-small text-neutral-base'>
-                    {data.board_type}
-                </div>}
-            <div className="flex justify-between">
-                <div className="flex flex-col">
-                    <h1 className="text-neutral-title text-subTitle">{data.title}</h1>
-                    <h2 className="text-neutral-base">{data.content}</h2>
-                </div>
-                <div className='min-w-20 min-h-20'>
-                    {data.image && <img src={data.image} alt="post image" className="object-cover w-20 h-20" />}
-                </div>
-            </div>
-            <div className="flex items-center justify-between">
-                <div className="flex gap-[0.375rem]">
-                    <div className="flex items-center gap-1 text-primary-red text-small">
-                        <Like />
-                        <p>{data.like}</p>
-                    </div>
-                    <div className="flex items-center gap-1 text-primary-30 text-small">
-                        <Comment />
-                        <p>{data.comment}</p>
-                    </div>
-                    <p className="text-neutral-border-50 text-small">{data.time}</p>
-                </div>
-                <Translate className="text-neutral-title" />
-            </div>
+  return (
+    <div className="flex flex-col gap-3 pt-4 pb-3">
+      {isActive /** 인기 게시판 레이아웃 */ && (
+        <div className="w-fit rounded-md bg-primary-10 px-[0.625rem] py-[0.3125rem] text-small text-neutral-base">
+          {data.board_type}
         </div>
-    )
-}
+      )}
+      <div className="flex justify-between gap-3">
+        <div className="flex flex-col line-clamp-3">
+          <h1 className="truncate text-subTitle text-neutral-title">{data.title}</h1>
+          <h2 className="line-clamp-2 text-neutral-base">{data.content}</h2>
+        </div>
+        {data.image && (
+          <div className="min-h-20 min-w-20">
+            <img
+              src={data.image}
+              alt="post image"
+              className="object-cover w-20 h-20"
+            />
+          </div>
+        )}
+      </div>
+      <div className="flex items-center justify-between">
+        <div className="flex gap-[0.375rem]">
+          <div className="flex items-center gap-1 text-small text-primary-red">
+            <Like />
+            <p>{data.like}</p>
+          </div>
+          <div className="flex items-center gap-1 text-small text-primary-30">
+            <Comment />
+            <p>{data.comment}</p>
+          </div>
+          <p className="text-small text-neutral-border-50">{data.time}</p>
+        </div>
+        <Translate className="text-neutral-title" />
+      </div>
+    </div>
+  );
+};
