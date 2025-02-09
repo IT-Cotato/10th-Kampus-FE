0a5ae78b3147fd247dc8db9d7da2095d7afad02f
seoyeon5117 <seoyun020207@gmail.com>
2025-02-10 03:09:16 +0900

    feat: 인증 코드 팝업 구현

--- src/pages/auth/schoolEmail.jsx
+++ src/pages/auth/schoolEmail.jsx
@@ -1,22 +1,31 @@
 import Prev from '@/assets/imgs/previous.svg?react';
+import { Alert } from '@/components/common/Alert';
 import { MainButton } from '@/components/common/MainButton';
 import { SkipButton } from '@/components/join/skipButtonShadow';
+import { VerificationCodeModal } from '@/components/join/VerificationCodeModal';
 import { path } from '@/routes/path';
 import { cn } from '@/utils/cn';
 import { useEffect, useState } from 'react';
+import { createPortal } from 'react-dom';
 import { useLocation, useNavigate } from 'react-router-dom';
 
 export const SchoolEmail = () => {
   const location = useLocation();
   const navigate = useNavigate();
   const [email, setEmail] = useState('');
-  const [verified, setVerified] = useState(true); // verify 실패 시 false
+  const [showModal, setShowModal] = useState(false);
 
   const university = location.state;
 
   const handleVerify = () => {
-    
-  }
+    setShowModal(true);
+    sendVerificationCode();
+  };
+
+  const sendVerificationCode = () => {
+    // 백 연동
+    console.log('코드 전송');
+  };
 
   useEffect(() => {
     if (university === undefined) {
@@ -43,12 +52,22 @@ export const SchoolEmail = () => {
           placeholder="School email address"
           onChange={(e) => setEmail(e.target.value)}
         />
-        <MainButton
-          onClick={handleVerify}
-          disabled={!email || !verified}
-        >
+        <MainButton onClick={handleVerify} disabled={!email}>
           Send Verification Code
         </MainButton>
+        {showModal &&
+          createPortal(
+            <VerificationCodeModal
+              title="Verifiy your Email"
+              text={`We just sent a code to ${email}. Please enter the code we sent to your email.`}
+              warningText={`That's not a valid verification code. Please try again.`}
+              leftButton="Cancel"
+              rightButton="Validate"
+              onClickLeft={() => setShowModal(false)}
+              resend={sendVerificationCode}
+            />,
+            document.getElementById('modal-root'),
+          )}
       </div>
     </div>
   );
