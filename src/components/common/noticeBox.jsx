3c49248f95100289e808ebe3fbb0df0d3f786dfa
“chae-dahee” <cdh010126r@gmail.com>
2025-02-12 00:25:25 +0900

    fix: noticeBox 기획에 따른 주석처리

--- src/components/common/noticeBox.jsx
+++ src/components/common/noticeBox.jsx
@@ -4,8 +4,9 @@ export const NoticeBox = () => {
   const navigate = useNavigate();
   return (
     <div
-      className="w-full rounded-[.625rem] bg-neutral-bg-10 h-[4.375rem] flex items-center justify-center text-subTitle text-neutral-[#6A6A6A] cursor-pointer mb-[1.25rem]"
-      onClick={() => navigate(`/my/service`, { state: { id: 2 } })}
+      className="text-neutral-[#6A6A6A] mb-[1.25rem] flex h-[4.375rem] w-full cursor-pointer items-center justify-center rounded-[.625rem] bg-neutral-bg-10 text-subTitle"
+      // 기획의도에 따라 수정
+      // onClick={() => navigate(`/my/service`)}
     >
       Notice
     </div>
