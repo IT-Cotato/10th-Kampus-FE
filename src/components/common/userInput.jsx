62ca419bd069c2284ffc2aaf874a2c8e6b07781d
“chae-dahee” <cdh010126r@gmail.com>
2025-02-12 00:12:12 +0900

    feat: 사용자 입력(댓글, 채팅) 개선

--- src/components/common/userInput.jsx
+++ src/components/common/userInput.jsx
@@ -1,26 +1,78 @@
-// @ts-ignore
 import Send from '@/assets/imgs/send.svg?react';
+import Camera from '@/assets/imgs/camera.svg?react';
 import { cn } from '@/utils/cn';
+import { useEffect, useRef } from 'react';
 
-export const UserInput = ({ placeholder, input, setInput, handleSend }) => {
+const InputTypes = {
+  CHAT: 'chat',
+};
+
+export const UserInput = ({
+  placeholder,
+  input,
+  setInput,
+  handleSend,
+  type,
+}) => {
+  const textareaRef = useRef(null);
+  const containerRef = useRef(null);
+  const maxHeight = 4 * 26;
+
+  const handleInput = () => {
+    // textarea 높이 조절
+    if (textareaRef.current) {
+      textareaRef.current.style.height = '0px';
+      const scrollHeight = textareaRef.current.scrollHeight;
+      textareaRef.current.style.height = `${Math.min(scrollHeight, maxHeight)}px`;
+
+      //div
+      if (containerRef.current) {
+        containerRef.current.style.height = `${Math.min(scrollHeight, maxHeight) + 20}px'`;
+      }
+    }
+  };
+
+  useEffect(() => {
+    handleInput();
+  }, [handleSend]);
+
   return (
-    <div className="bottom-0 flex min-h-[3rem] resize-none items-center justify-end bg-neutral-bg-5 px-4 py-1">
+    <div
+      ref={containerRef}
+      className="fixed bottom-3 left-0 right-0 mx-4 flex h-auto items-start overflow-y-auto rounded-[1.25rem] bg-neutral-bg-5 px-2 py-2"
+    >
+      {type === InputTypes.CHAT && (
+        <button
+          // onClick={handlePhoto}
+          aria-label="사진파일 전송"
+          className={cn('left-6 h-8 w-8', {
+            'text-primary-30': input,
+            'text-neutral-border-30': !input,
+          })}
+        >
+          <Camera className="h-full w-full text-neutral-icon" />
+        </button>
+      )}
       <textarea
+        ref={textareaRef}
         value={input}
-        onChange={(e) => setInput(e.target.value)}
-        className="h-full w-full rounded-[1.25rem] border border-gray-300 bg-transparent px-2 text-small text-neutral-title"
+        onChange={(e) => {
+          setInput(e.target.value);
+          handleInput();
+        }}
+        rows={1}
+        className="flex-grow resize-none bg-neutral-bg-5 px-2 pt-1 text-base text-neutral-title"
         placeholder={placeholder}
       />
       <button
-        onClick={handleSend}
+        onClick={() => {
+          handleSend();
+        }}
         aria-label="메시지 전송"
-        className={cn(
-          'absolute mr-2 flex h-8 w-8 items-center justify-center',
-          {
-            'text-primary-30': input,
-            'text-neutral-border-30': !input,
-          },
-        )}
+        className={cn('right-6 h-8 w-8', {
+          'text-primary-30': input,
+          'text-neutral-border-30': !input,
+        })}
       >
         <Send className="h-full w-full" />
       </button>
