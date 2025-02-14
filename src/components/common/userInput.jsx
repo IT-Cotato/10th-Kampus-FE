8c0191fd006e30342cbcb81a4c76292b1a6dd3cf
“chae-dahee” <cdh010126r@gmail.com>
2025-02-14 16:04:19 +0900

    fix: textarea 너비 조정

--- src/components/common/userInput.jsx
+++ src/components/common/userInput.jsx
@@ -37,45 +37,47 @@ export const UserInput = ({
   }, [handleSend]);
 
   return (
-    <div
-      ref={containerRef}
-      className="fixed bottom-3 left-0 right-0 mx-4 flex h-auto items-start overflow-y-auto rounded-[1.25rem] bg-neutral-bg-5 px-2 py-2"
-    >
-      {type === InputTypes.CHAT && (
+    <div className="fixed bottom-3 left-0 right-0 mx-auto max-w-lg px-4">
+      <div
+        ref={containerRef}
+        className="h-autoitems-start flex overflow-y-auto rounded-[1.25rem] bg-neutral-bg-5 px-2 py-2"
+      >
+        {type === InputTypes.CHAT && (
+          <button
+            // onClick={handlePhoto}
+            aria-label="사진파일 전송"
+            className={cn('left-6 h-8 w-8', {
+              'text-primary-30': input,
+              'text-neutral-border-30': !input,
+            })}
+          >
+            <Camera className="h-full w-full text-neutral-icon" />
+          </button>
+        )}
+        <textarea
+          ref={textareaRef}
+          value={input}
+          onChange={(e) => {
+            setInput(e.target.value);
+            handleInput();
+          }}
+          rows={1}
+          className="flex-grow resize-none bg-neutral-bg-5 px-2 pt-1 text-base text-neutral-title"
+          placeholder={placeholder}
+        />
         <button
-          // onClick={handlePhoto}
-          aria-label="사진파일 전송"
-          className={cn('left-6 h-8 w-8', {
+          onClick={() => {
+            handleSend();
+          }}
+          aria-label="메시지 전송"
+          className={cn('right-6 h-8 w-8', {
             'text-primary-30': input,
             'text-neutral-border-30': !input,
           })}
         >
-          <Camera className="w-full h-full text-neutral-icon" />
+          <Send className="h-full w-full" />
         </button>
-      )}
-      <textarea
-        ref={textareaRef}
-        value={input}
-        onChange={(e) => {
-          setInput(e.target.value);
-          handleInput();
-        }}
-        rows={1}
-        className="flex-grow px-2 pt-1 text-base resize-none bg-neutral-bg-5 text-neutral-title"
-        placeholder={placeholder}
-      />
-      <button
-        onClick={() => {
-          handleSend();
-        }}
-        aria-label="메시지 전송"
-        className={cn('right-6 h-8 w-8', {
-          'text-primary-30': input,
-          'text-neutral-border-30': !input,
-        })}
-      >
-        <Send className="w-full h-full" />
-      </button>
+      </div>
     </div>
   );
 };
