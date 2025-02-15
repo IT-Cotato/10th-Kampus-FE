05cda613f21d9f8124381129ec8ec2683c322171
seoyeon5117 <seoyun020207@gmail.com>
2025-02-16 02:20:30 +0900

    fix: 대소문제 에러 해결

--- src/pages/index.js
+++ src/pages/index.js
@@ -37,7 +37,7 @@ export { MyArticles } from './my/community/MyArticles';
 export { BlockChat } from './my/etc/blockChat';
 export { BlockSecondhand } from './my/etc/blockSecondhand';
 export { DeleteAccount } from './my/etc/deleteAccount';
-export { MyInfo } from './my/settings/myInfo';
+export { MyInfo } from './my/settings/MyInfo';
 export { Notification } from './my/settings/Notification';
 export { Search } from './search/Search';
 
