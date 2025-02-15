2a6c87d23868ab557a36afbf0d000943e4072cf7
seoyeon5117 <seoyun020207@gmail.com>
2025-02-15 17:01:37 +0900

    feat: 관리자페이지 레이아웃 햄버거바 추가

--- src/components/layout/Admin.jsx
+++ src/components/layout/Admin.jsx
@@ -1,12 +1,16 @@
 import { path } from '@/routes/path';
 import { cn } from '@/utils/cn';
+import { useState } from 'react';
 import { useLocation, useNavigate } from 'react-router-dom';
+import HamburgerIcon from '@/assets/imgs/admin/HamburgerIcon.png';
 
 export const Admin = ({ children }) => {
   const location = useLocation();
   const { pathname } = location;
 
   const navigate = useNavigate();
+
+  const [showMenu, setShowMenu] = useState(true);
 
   const menu = [
     { id: 0, text: '대시보드', path: path.admin.dashboard },
@@ -20,28 +24,37 @@ export const Admin = ({ children }) => {
 
   return (
     <div className="flex min-h-dvh w-full min-w-[64rem]">
-      <div className="flex justify-center text-center align-middle w-72">
-        <span className="absolute hidden left-10 top-5 text-pageTitle text-primary-90 lg:flex">
+      <span className="absolute flex flex-row items-center gap-2 left-2 top-2">
+        <button
+          className="w-10 h-10 bg-white bg-cover rounded-lg"
+          style={{ backgroundImage: `url(${HamburgerIcon})` }}
+          onClick={() => setShowMenu(!showMenu)}
+        ></button>
+        <span className="hidden text-pageTitle text-primary-90 lg:flex">
           Kampus
         </span>
-        <div className="flex flex-col justify-center text-start">
-          {menu.map((item) => (
-            <button
-              key={item.id}
-              className={cn(
-                'box-border h-16 w-72 px-10 text-start text-subTitle text-neutral-base',
-                {
-                  'border-l-4 border-primary-base text-primary-base':
-                    pathname.includes(item.path),
-                },
-              )}
-              onClick={() => navigate(`./${item.path}`, { replace: true })}
-            >
-              {item.text}
-            </button>
-          ))}
+      </span>
+      {showMenu && (
+        <div className="flex flex-row justify-center text-center align-middle w-72">
+          <div className="flex flex-col justify-center text-start">
+            {menu.map((item) => (
+              <button
+                key={item.id}
+                className={cn(
+                  'box-border h-16 w-72 px-10 text-start text-subTitle text-neutral-base',
+                  {
+                    'border-l-4 border-primary-base text-primary-base':
+                      pathname.includes(item.path),
+                  },
+                )}
+                onClick={() => navigate(`./${item.path}`, { replace: true })}
+              >
+                {item.text}
+              </button>
+            ))}
+          </div>
         </div>
-      </div>
+      )}
       <div className="flex flex-col w-full">
         <div className="justify-end hidden w-full px-6 py-2 bg-white lg:flex">
           <span className="px-4 py-1 border-2 rounded-full border-primary-40">
