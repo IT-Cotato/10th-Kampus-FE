2724c9840c4f5956025c6b91166488129a22228f
seoyeon5117 <seoyun020207@gmail.com>
2025-02-18 15:02:29 +0900

    feat: 삭제 대기 항목 추가, dropdown 컴포넌트로 분리

---
+++ src/components/admin/Dropdown.jsx
@@ -0,0 +1,75 @@
+// @ts-nocheck
+
+import { cn } from '@/utils/cn';
+
+/**
+ * 드롭다운 컴포넌트
+ *
+ * @param {Object} props 컴포넌트 Props
+ * @param {string} selectedDropdown 현재 선택된 드롭다운 항목(useState 상태값)
+ * @param {string[]} dropdownOptions 드롭다운에서 선택할 수 있는 항목(배열)
+ * @param {boolean} isDropdownOpen 드롭다운이 열려 있는지 여부를 나타내는 상태값
+ * @param {(open: boolean) => void} setIsDropdownOpen 드롭다운 열림/닫힘 상태를 변경하는 함수 (useState 상태 업데이트 함수)
+ * @param {function} handleDropdownClick 드롭다운 항목 클릭 시 실행되는 콜백 함수
+ */
+export const Dropdown = ({
+  selectedDropdown,
+  dropdownOptions,
+  isDropdownOpen,
+  setIsDropdownOpen,
+  handleDropdownClick,
+}) => {
+  return (
+    <>
+      <button
+        id="dropdownDefaultButton"
+        data-dropdown-toggle="dropdown"
+        className="inline-flex w-fit items-center rounded-lg border border-primary-base px-5 py-2.5 text-center font-medium focus:outline-none focus:ring-4 focus:ring-primary-20"
+        type="button"
+        onClick={() => setIsDropdownOpen(!isDropdownOpen)}
+      >
+        {selectedDropdown}{' '}
+        <svg
+          className="ms-3 h-2.5 w-2.5"
+          aria-hidden="true"
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 10 6"
+        >
+          <path
+            stroke="currentColor"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth="2"
+            d="m1 1 4 4 4-4"
+            className={cn('text-primary-base', {
+              'translate-x-full translate-y-full rotate-180': isDropdownOpen,
+            })}
+          />
+        </svg>
+      </button>
+      <div
+        id="dropdown"
+        className={cn(
+          'z-10 hidden w-fit min-w-28 rounded-lg border border-primary-base bg-white',
+          {
+            block: isDropdownOpen,
+          },
+        )}
+      >
+        <ul
+          className="py-2 divide-y divide-primary-20 text-neutral-80"
+          aria-labelledby="dropdownDefaultButton"
+        >
+          {dropdownOptions.map((state, index) => (
+            <li key={index} onClick={() => handleDropdownClick(state)}>
+              <button className="flex w-full px-4 py-2 hover:bg-primary-10">
+                {state}
+              </button>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </>
+  );
+};
--- src/pages/admin/BoardManagement.jsx
+++ src/pages/admin/BoardManagement.jsx
@@ -1,19 +1,19 @@
+import { Dropdown } from '@/components/admin/Dropdown';
 import { path } from '@/routes/path';
-import { cn } from '@/utils/cn';
 import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 export const BoardManagement = () => {
   const navigate = useNavigate();
-  const BoardState = ['전체', '활성화', '보관'];
-  const [boardState, setBoardState] = useState('전체');
+  const BoardOptions = ['전체', '활성화', '보관', '삭제 대기'];
+  const [selectedDropdown, setSelectedDropdown] = useState('전체');
   const [isDropdownOpen, setIsDropdownOpen] = useState(false);
   const [boardList, setBoardList] = useState([]);
 
   const handleDropdownClick = (state) => {
-    setBoardState(state);
+    setSelectedDropdown(state);
     setIsDropdownOpen(false);
-  }
+  };
 
   useEffect(() => {
     // 백 연동
@@ -24,6 +24,7 @@ export const BoardManagement = () => {
         postNum: 30,
         description:
           'Share the information about dorms, rental rooms, and shared housing near campus!',
+        dday: 1,
       },
       {
         id: 1,
@@ -31,6 +32,7 @@ export const BoardManagement = () => {
         postNum: 30,
         description:
           'Find part-time job and employment opportunities in Korea.',
+        dday: 0,
       },
       {
         id: 2,
@@ -38,12 +40,14 @@ export const BoardManagement = () => {
         postNum: 30,
         description:
           'Connect with others for language exchange and cultural learning.',
+        dday: 10,
       },
       {
         id: 3,
         title: 'Festival/Events',
         postNum: 30,
         description: 'Discover upcoming festivals and events in Korea.',
+        dday: 10,
       },
       {
         id: 4,
@@ -51,6 +55,7 @@ export const BoardManagement = () => {
         postNum: 30,
         description:
           'Find essential updates and helpful resources for living, studying, and working in Korea.',
+        dday: 27,
       },
       {
         id: 5,
@@ -58,6 +63,7 @@ export const BoardManagement = () => {
         postNum: 30,
         description:
           'Got questions? Get answers from fellow international students and expats in Korea.',
+        dday: 15,
       },
       {
         id: 6,
@@ -65,6 +71,7 @@ export const BoardManagement = () => {
         postNum: 30,
         description:
           'Chat about anything and everything! Share your experiences, thoughts, and daily life with the community.',
+        dday: 10,
       },
       {
         id: 7,
@@ -72,64 +79,22 @@ export const BoardManagement = () => {
         postNum: 30,
         description:
           'Stay updated with the hottest topics and discussions happening right now in the community.',
+        dday: 10,
       },
     ]);
-  }, [boardState]);
+  }, [selectedDropdown]);
 
   return (
     <div className="flex flex-col flex-1 gap-5 px-5">
       <div className="flex flex-col w-full gap-5 p-8 bg-white h-fit rounded-2xl">
-        게시판 상태
-        <br />
-        <button
-          id="dropdownDefaultButton"
-          data-dropdown-toggle="dropdown"
-          className="inline-flex w-fit items-center rounded-lg border border-primary-base px-5 py-2.5 text-center font-medium focus:outline-none focus:ring-4 focus:ring-primary-20"
-          type="button"
-          onClick={() => setIsDropdownOpen(!isDropdownOpen)}
-        >
-          {boardState}{' '}
-          <svg
-            className="ms-3 h-2.5 w-2.5"
-            aria-hidden="true"
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 10 6"
-          >
-            <path
-              stroke="currentColor"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              d="m1 1 4 4 4-4"
-              className={cn('', {
-                'translate-x-full translate-y-full rotate-180': isDropdownOpen,
-              })}
-            />
-          </svg>
-        </button>
-        <div
-          id="dropdown"
-          className={cn(
-            'z-10 hidden w-fit min-w-28 divide-y divide-gray-300 rounded-lg border border-primary-base bg-white',
-            {
-              block: isDropdownOpen,
-            },
-          )}
-        >
-          <ul
-            className="py-2 text-neutral-80"
-            aria-labelledby="dropdownDefaultButton"
-          >
-            {BoardState.map((state, index) => (
-              <li key={index} onClick={() => handleDropdownClick(state)}>
-                <a href="#" className="block px-4 py-2 hover:bg-primary-10">
-                  {state}
-                </a>
-              </li>
-            ))}
-          </ul>
-        </div>
+        <h1 className="text-pageTitle">게시판 상태</h1>
+        <Dropdown
+          selectedDropdown={selectedDropdown}
+          dropdownOptions={BoardOptions}
+          isDropdownOpen={isDropdownOpen}
+          setIsDropdownOpen={setIsDropdownOpen}
+          handleDropdownClick={(state) => handleDropdownClick(state)}
+        />
       </div>
       <div className="grid w-full grid-cols-[repeat(auto-fill,_minmax(15rem,_1fr))] place-items-center gap-7 lg:grid-cols-[repeat(auto-fill,_minmax(18.75rem,_1fr))]">
         {boardList.map((board, index) => (
@@ -138,10 +103,12 @@ export const BoardManagement = () => {
             className="relative flex h-40 min-h-fit w-60 min-w-fit flex-col gap-5 rounded-2xl bg-white p-8 lg:h-[12.5rem] lg:w-[18.75rem]"
           >
             <div className="flex flex-row items-center gap-3 text-center align-middle">
-              <span className="px-2 rounded-lg h-fit w-fit bg-primary-10 text-subTitle text-primary-base">
-                {index}
+              <span className="px-2 rounded-lg h-fit w-fit whitespace-nowrap bg-primary-10 text-subTitle text-primary-base">
+                {selectedDropdown === '삭제 대기'
+                  ? 'D-' + (board.dday === 0 ? 'day' : board.dday)
+                  : index}
               </span>
-              <h2>
+              <h2 className="whitespace-nowrap">
                 {board.title} | {board.postNum}개
               </h2>
             </div>
