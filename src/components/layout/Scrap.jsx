8eb69a4325dff1a56e42c7e16d44a72eb95c486a
seoyeon5117 <seoyun020207@gmail.com>
2025-02-18 23:40:46 +0900

    chore: button round 적용 안된 부분 수정

--- src/components/layout/Scrap.jsx
+++ src/components/layout/Scrap.jsx
@@ -10,24 +10,23 @@ export const Scrap = ({ children }) => {
   const navigate = useNavigate();
 
   const menu = [
-    { id: 0, text: 'Community', path: path.mypage.community.scrap.community},
+    { id: 0, text: 'Community', path: path.mypage.community.scrap.community },
     { id: 1, text: 'Secondhand', path: path.mypage.community.scrap.secondhand },
   ];
 
   return (
     <div className="flex flex-col w-full">
       <TitleHeader text="Scrap" />
-      <div className="flex flex-row w-full px-4 pt-12 pb-6 gap-[0.625rem] justify-start">
-          {menu.map((item) => (
-            <ButtonRound
-              key={item.id}
-              theme={`${pathname.includes(item.path) ? 'primary' : 'disabled'}`}
-              text={item.text}
-              width="base"
-              height="small"
-              onClick={() => navigate(`./${item.path}`, { replace: true })}
-            />
-          ))}
+      <div className="flex w-full flex-row justify-start gap-[0.625rem] px-4 pb-6 pt-12">
+        {menu.map((item) => (
+          <ButtonRound
+            key={item.id}
+            theme={`${pathname.includes(item.path) ? 'primary' : 'disabled'}`}
+            text={item.text}
+            size="short"
+            onClick={() => navigate(`./${item.path}`, { replace: true })}
+          />
+        ))}
       </div>
       <div className="flex flex-col flex-1 w-full px-4">{children}</div>
     </div>
