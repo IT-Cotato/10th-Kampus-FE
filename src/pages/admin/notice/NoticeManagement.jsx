6465c2417c69da76ef3453cb4a08103cf623e5d0
seoyeon5117 <seoyun020207@gmail.com>
2025-02-20 04:52:27 +0900

    chore: thead, tbody 추가

--- src/pages/admin/notice/NoticeManagement.jsx
+++ src/pages/admin/notice/NoticeManagement.jsx
@@ -10,15 +10,23 @@ export const NoticeManagement = () => {
   useEffect(() => {
     // 백 연동
     setNoticeList([
-      { createdDate: 2024121232, title: '[EVENT] New board is opened', noticeId: 1234 },
-      { createdDate: 2024121232, title: '[EVENT] New board is opened', noticeId: 1255 },
+      {
+        createdDate: 2024121232,
+        title: '[EVENT] New board is opened',
+        noticeId: 1234,
+      },
+      {
+        createdDate: 2024121232,
+        title: '[EVENT] New board is opened',
+        noticeId: 1255,
+      },
     ]);
   }, []);
 
   const handleDeleteNotice = (noticeId) => {
     alert('공지사항이 삭제되었습니다.');
     // 백 연동
-  }
+  };
 
   return (
     <div className="flex flex-col flex-1 gap-5 px-5">
@@ -32,41 +40,45 @@ export const NoticeManagement = () => {
       <div className="relative flex flex-col w-full h-full gap-5 p-8 text-base bg-white rounded-2xl text-neutral-title">
         <h1 className="text-subTitle">공지사항 리스트</h1>
         <table className="divide-y divide-primary-20">
-          <tr className="h-8 text-primary-base">
-            <th scope="col" className="text-start">
-              공지 등록일
-            </th>
-            <th scope="col" className="text-start">
-              공지 제목
-            </th>
-            <th scope="col" className="text-start">
-              수정
-            </th>
-            <th scope="col" className="text-start">
-              삭제
-            </th>
-          </tr>
+          <thead>
+            <tr className="h-8 text-primary-base">
+              <th scope="col" className="text-start">
+                공지 등록일
+              </th>
+              <th scope="col" className="text-start">
+                공지 제목
+              </th>
+              <th scope="col" className="text-start">
+                수정
+              </th>
+              <th scope="col" className="text-start">
+                삭제
+              </th>
+            </tr>
+          </thead>
           {noticeList &&
             noticeList.map((notice, index) => (
-              <tr key={index} className='h-12'>
-                <td>{notice.createdDate}</td>
-                <td>{notice.title}</td>
-                <td>
-                  <ButtonRound
-                    text="수정하기"
-                    size="short"
-                    onClick={() => navigate(path.admin.notice.edit)}
-                  />
-                </td>
-                <td>
-                  <ButtonRound
-                    text="삭제하기"
-                    theme="border"
-                    size="short"
-                    onClick={() => handleDeleteNotice(notice.noticeId)}
-                  />
-                </td>
-              </tr>
+              <tbody key={index}>
+                <tr className="h-12">
+                  <td>{notice.createdDate}</td>
+                  <td>{notice.title}</td>
+                  <td>
+                    <ButtonRound
+                      text="수정하기"
+                      size="short"
+                      onClick={() => navigate(path.admin.notice.edit)}
+                    />
+                  </td>
+                  <td>
+                    <ButtonRound
+                      text="삭제하기"
+                      theme="border"
+                      size="short"
+                      onClick={() => handleDeleteNotice(notice.noticeId)}
+                    />
+                  </td>
+                </tr>
+              </tbody>
             ))}
         </table>
       </div>
