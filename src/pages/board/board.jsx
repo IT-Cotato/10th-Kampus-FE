4ee5ecfedc353edf147b5dd8cc00d626412e8d4f
Ki-Tak <lee08tain@gmail.com>
2025-02-19 14:33:23 +0900

    fix: notice -> board guide 디자인 수정

--- src/pages/board/board.jsx
+++ src/pages/board/board.jsx
@@ -72,9 +72,9 @@ export const Board = () => {
       <PostHeader path={path} />
       <div className="flex flex-col w-full h-full pt-14">
         <div className="flex w-full flex-col gap-[0.875rem] bg-white px-4 pb-1 pt-5">
-          <div className="flex w-full cursor-pointer items-center justify-center rounded-[0.625rem] bg-neutral-bg-10 py-3 text-subTitle font-normal text-[#6A6A6A]"
+          <div className="flex w-full cursor-pointer items-center justify-center rounded-[0.625rem] bg-primary-10 py-2 text-small text-neutral-base"
             onClick={() => navigate(path.boardGuide)}>
-            Notice
+            Board guide
           </div>
           {isActive.filter && <FilterBox />}
           {/** 추후, 백엔드와 필터 작업 시 props 넘겨줘야 함 */}
