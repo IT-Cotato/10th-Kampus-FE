52a7864f4717b7b397c39077f97ed5cc4619e99b
seoyeon5117 <seoyun020207@gmail.com>
2025-02-21 05:22:35 +0900

    feat: 공지 목록 로딩 추가

--- src/pages/my/service/notice/notice.jsx
+++ src/pages/my/service/notice/notice.jsx
@@ -1,24 +1,29 @@
 import { getNoticeList } from '@/apis/mypage/getNoticeList.api';
+import { Loading } from '@/components/common/Loading';
 import { NoticeList } from '@/components/service/notice/NoticeList';
 import { QUERY_KEYS } from '@/constants/api';
 import { useQuery } from '@tanstack/react-query';
 
 export const Notice = () => {
-  const { data: noticeList } = useQuery({
+  const { data: noticeList, isLoading } = useQuery({
     queryKey: [QUERY_KEYS.GET_NOTICE],
     queryFn: () => getNoticeList(),
   });
 
   return (
     <div className="flex flex-col w-full">
-      {noticeList && noticeList.notices.map((notice) => (
-        <NoticeList
-          key={notice.id}
-          noticeId={notice.id}
-          title={notice.title}
-          date={notice.date}
-        />
-      ))}
+      {isLoading ? (
+        <Loading />
+      ) : (
+        noticeList.notices.map((notice) => (
+          <NoticeList
+            key={notice.id}
+            noticeId={notice.id}
+            title={notice.title}
+            date={notice.date}
+          />
+        ))
+      )}
     </div>
   );
 };
