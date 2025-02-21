da7116c9932580b1bbda0af84453e651d8d30c33
seoyeon5117 <seoyun020207@gmail.com>
2025-02-21 11:29:00 +0900

    fix: 학교 이메일 인증 쿼리명 universityId -> universityName으로 변경

--- src/apis/auth/postSchoolPhoto.api.js
+++ src/apis/auth/postSchoolPhoto.api.js
@@ -1,11 +1,12 @@
 import { authApi } from '../axios-instance';
 import { API_DOMAINS } from '@/constants/api';
-export const postSchoolPhoto = async ({ data, universityId }) => {
+
+export const postSchoolPhoto = async ({ data, universityName }) => {
   const response = await authApi.post(API_DOMAINS.VERIFY_SCHOOL_PHOTO, data, {
     headers: {
       'Content-Type': 'multipart/form-data',
     },
-    params: { universityId },
+    params: { universityName },
   });
   return response.data.data;
 };
