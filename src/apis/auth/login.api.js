c23678631922e19808ceb2a85bb58b661738cecc
Ki-Tak <lee08tain@gmail.com>
2025-02-21 13:05:54 +0900

    fix: cor 에러 및 로그인 임시 해결

--- src/apis/auth/login.api.js
+++ src/apis/auth/login.api.js
@@ -1,7 +1,6 @@
 import axios from 'axios';
 import { authApi } from '../axios-instance';
 import { API_DOMAINS } from '@/constants/api';
-
 export const postLogin = async () => {
   const { data } = await axios.post('dummy');
   return data;
@@ -9,7 +8,7 @@ export const postLogin = async () => {
 
 export const getUserDetail = async () => {
   const response = await authApi.get(API_DOMAINS.USER);
-  return { success: true, data: response.data.data };
+  return { success: true, data: response.data?.data };
 };
 export const patchSignup = async (signupData) => {
   const response = await authApi.patch(API_DOMAINS.USER, {
