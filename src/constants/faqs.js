ae7627144477a55a2c426866947e8e0c3abc1f0c
“chae-dahee” <cdh010126r@gmail.com>
2025-02-21 13:39:55 +0900

    feat: faq 데이터

--- src/constants/faqs.js
+++ src/constants/faqs.js
@@ -1,16 +1,32 @@
 export const faqs = [
   {
-    question: 'Where can I change user name?',
+    question: 'What is Kampus?',
+    answer:
+      'Kampus is a global community platform for international students, designed to help them connect, share information, and communicate easily through posts and chats.',
+  },
+  {
+    question: 'What language should I use when writing posts?',
+    answer:
+      'You can write posts in any language, but Kampus encourages the use of English. To make communication easier, you can use the "Upload & Translate" feature to automatically translate your post into English before uploading.',
+  },
+  {
+    question: 'How does the "Upload & Translate" feature work?',
+    answer:
+      'After writing a post, click "Upload & Translate" to see a preview of the English translation. Once you confirm it, the translated post will be uploaded. This feature helps users communicate more effectively in a shared language.',
+  },
+  {
+    question: 'Are chats anonymous?',
     answer:
       'Lorem ipsum dolor sit amet consectetur. Nisi id placerat tortor orci quis volutpat. Diam adipiscing tempor a lacinia mattis vel ut. Suspendisse mauris at in eget lectus aliquam eget adipiscing nunc. Venenatis dui nibh porta elit tellus dignissim nisl.',
   },
   {
-    question: 'Where can I change user name?',
+    question: 'Can I block users in the chat?',
     answer:
       'Lorem ipsum dolor sit amet consectetur. Nisi id placerat tortor orci quis volutpat. Diam adipiscing tempor a lacinia mattis vel ut. Suspendisse mauris at in eget lectus aliquam eget adipiscing nunc. Venenatis dui nibh porta elit tellus dignissim nisl.',
   },
   {
-    question: 'Where can I change user name?',
+    question:
+      'Can I block users in the chat?I didn’t verify my school when signing up. Can I do it later?',
     answer:
       'Lorem ipsum dolor sit amet consectetur. Nisi id placerat tortor orci quis volutpat. Diam adipiscing tempor a lacinia mattis vel ut. Suspendisse mauris at in eget lectus aliquam eget adipiscing nunc. Venenatis dui nibh porta elit tellus dignissim nisl.',
   },
