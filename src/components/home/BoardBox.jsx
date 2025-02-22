29088213690e54958b4b2846e00e25e727665826
seoyeon5117 <seoyun020207@gmail.com>
2025-02-22 20:14:08 +0900

    chore: home의 BoardBox min-h 없애기

--- src/components/home/BoardBox.jsx
+++ src/components/home/BoardBox.jsx
@@ -1,11 +1,11 @@
 import { BoxList } from './BoxList';
 import { BoardHeader } from './BoardHeader';
 import { CardPost } from './BoxList';
-export const BoardBox = ({ boardTitle, data, path }) => {
+export const BoardBox = ({ boardTitle, data }) => {
   return (
     <div className="flex flex-col gap-5">
       <BoardHeader title={boardTitle} />
-      <div className="flex min-h-[16.875rem] w-full flex-col rounded-[.625rem] bg-primary-5 px-5 py-[.625rem]">
+      <div className="flex h-fit w-full flex-col rounded-[.625rem] bg-primary-5 px-5 py-[.625rem]">
         {data?.map((text, index) => (
           <BoxList key={index} text={text} />
         ))}
@@ -13,11 +13,11 @@ export const BoardBox = ({ boardTitle, data, path }) => {
     </div>
   );
 };
-export const CardPostBox = ({ data, path }) => {
+export const CardPostBox = ({ data }) => {
   return (
     <div className="flex w-full flex-col gap-[1.875rem] overflow-hidden">
       <BoardHeader title="How to live in Korea" />
-      <div className="grid shrink-0 grid-flow-col gap-4 overflow-x-auto scroll-smooth scrollbar-hide">
+      <div className="grid grid-flow-col gap-4 overflow-x-auto cursor-pointer shrink-0 scroll-smooth scrollbar-hide">
         {data?.map((item, index) => (
           <CardPost key={index} data={item} />
         ))}
