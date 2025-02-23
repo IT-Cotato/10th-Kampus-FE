110ff974fe10c46151ef83c268382ada0ca865ba
seoyeon5117 <seoyun020207@gmail.com>
2025-02-23 14:45:13 +0900

    chore: Board에서 보드 없으면 보라 테두리만 렌더링되는 부분 해결

--- src/components/board/BoardListBox.jsx
+++ src/components/board/BoardListBox.jsx
@@ -1,18 +1,21 @@
 import { BoardList } from './BoardList';
+
 export const BoardListBox = ({ list, listKey, togglePin }) => {
+  let boardNum = list.length;
+
+  if (boardNum === 0) return null;
+
   return (
     <div className="flex w-full flex-col rounded-lg bg-white border-[0.5px] border-primary-base">
-      {list.map((data, index) => {
-        return (
-          <BoardList
-            data={data}
-            key={index}
-            listKey={listKey}
-            index={index}
-            togglePin={togglePin}
-          />
-        );
-      })}
+      {list.map((data, index) => (
+        <BoardList
+          data={data}
+          key={index}
+          listKey={listKey}
+          index={index}
+          togglePin={togglePin}
+        />
+      ))}
     </div>
   );
 };
