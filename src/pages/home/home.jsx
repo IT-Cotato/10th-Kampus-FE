b8c8bb3093eefc879ebe592e975f4c9612349cee
seoyeon5117 <seoyun020207@gmail.com>
2025-02-23 13:46:02 +0900

    feat: trending 없으면 홈에서 안보이게 수정

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -74,7 +74,7 @@ export const Home = () => {
           data={favoriteList?.previewList}
           boardTitle="Favorites"
         />
-        <BoardBox data={trendingList?.previewList} boardTitle="Trending" />
+        {trendingList.previewList.length !== 0 && <BoardBox data={trendingList?.previewList} boardTitle="Trending" />}
         <CardPostBox data={cardNewsList?.posts} />
       </div>
     </div>
