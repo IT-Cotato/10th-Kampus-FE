76c43945b377cc8356ca82536734475f29b486ec
seoyeon5117 <seoyun020207@gmail.com>
2025-04-24 15:44:48 +0900

    design: 게시글 업로드 버튼 디자인 변경

--- src/components/board/write/WriteTitle.jsx
+++ src/components/board/write/WriteTitle.jsx
@@ -1,7 +1,6 @@
 export const WriteTitle = ({ title, setTitle, placeholder, maxLength = 0 }) => {
-
   const handleOnChange = (e) => {
-    if(maxLength && e.target.value.length > maxLength) {
+    if (maxLength && e.target.value.length > maxLength) {
       e.target.value = e.target.value.slice(0, maxLength);
     }
     setTitle(e.target.value);
@@ -12,18 +11,22 @@ export const WriteTitle = ({ title, setTitle, placeholder, maxLength = 0 }) => {
       <label htmlFor="writeTitle" className="text-subTitle">
         Title
       </label>
-      <div className="box-border flex w-full flex-row rounded-lg border border-neutral-border-30 px-[.875rem] py-[1.125rem] gap-2">
+      <div className="relative h-fit w-full">
         <input
+          className="box-border flex w-full flex-row gap-2 rounded-lg border border-neutral-border-30 py-[1.125rem] pl-[.875rem] pr-16 leading-none placeholder-neutral-border-50"
           id="writeTitle"
           type="text"
           placeholder={placeholder}
           value={title}
           onChange={handleOnChange}
           autoComplete="off"
-          className="w-full leading-none placeholder-neutral-border-50"
           required
         />
-        {maxLength !== 0 && <span className="flex justify-end text-sm w-fit text-neutral-border-50">{title.length}/{maxLength}</span>}
+        {maxLength !== 0 && (
+          <span className="text-sm absolute right-[.875rem] top-[1.125rem] flex w-fit text-neutral-border-50">
+            {title.length}/{maxLength}
+          </span>
+        )}
       </div>
     </div>
   );
--- src/components/common/MainWhiteButton.jsx
+++ src/components/common/MainWhiteButton.jsx
@@ -7,7 +7,7 @@ export const MainWhiteButton = (props) => {
       onClick={props.onClick}
       disabled={props.disabled}
       className={cn(
-        'box-border h-[3.6875rem] w-full cursor-pointer rounded-[.625rem] border border-primary-base bg-white text-primary-base',
+        'box-border h-[3.6875rem] w-full cursor-pointer rounded-[.625rem] border border-primary-base bg-white px-[1.125rem] text-primary-base',
         {
           'cursor-default border-neutral-border-40 text-neutral-border-40':
             props.disabled,
