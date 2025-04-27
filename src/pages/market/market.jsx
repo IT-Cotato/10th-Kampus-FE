27d4aeaed874ac6ed403aa443874c531a916d702
seoyeon5117 <seoyun020207@gmail.com>
2025-04-27 20:36:33 +0900

    feat: 중고거래 게시글 없을 경우 UI 구현

--- src/pages/market/market.jsx
+++ src/pages/market/market.jsx
@@ -12,26 +12,26 @@ import { MarketList } from '@/components/board/MarketList';
 export const Market = () => {
   const postList = {
     posts: [
-      {
-        postId: 1,
-        title: '인형',
-        price: 30000,
-        likes: 10,
-        comments: 0,
-        thumbnailUrl: 'src/assets/imgs/bg1.png',
-        isScrapped: true,
-        createdTime: '2025-04-27 02:56:52',
-      },
-      {
-        postId: 2,
-        title: '인형',
-        price: 20000,
-        likes: 10,
-        comments: 0,
-        thumbnailUrl: 'src/assets/imgs/bg2.png',
-        isScrapped: false,
-        createdTime: '2025-04-27 02:56:52',
-      },
+      //   {
+      //     postId: 1,
+      //     title: '인형',
+      //     price: 30000,
+      //     likes: 10,
+      //     comments: 0,
+      //     thumbnailUrl: 'src/assets/imgs/bg1.png',
+      //     isScrapped: true,
+      //     createdTime: '2025-04-27 02:56:52',
+      //   },
+      //   {
+      //     postId: 2,
+      //     title: '인형',
+      //     price: 20000,
+      //     likes: 10,
+      //     comments: 0,
+      //     thumbnailUrl: 'src/assets/imgs/bg2.png',
+      //     isScrapped: false,
+      //     createdTime: '2025-04-27 02:56:52',
+      //   },
     ],
   };
 
@@ -46,19 +46,24 @@ export const Market = () => {
   return (
     <div className="flex flex-1">
       {/* <PostHeader path={path} /> */}
-      <div className="relative flex flex-1 flex-col pb-14 pt-14">
+      <div className="relative flex flex-1 flex-col pt-14">
         <div className="flex w-full flex-col gap-[0.875rem] bg-white px-4 pb-1 pt-5">
           {boardDetail.filter && <FilterBox />}
           {/** 추후, 백엔드와 필터 작업 시 props 넘겨줘야 함 */}
         </div>
-        <div className="flex w-full flex-col divide-y bg-white px-4">
+        <div className="flex w-full flex-1 flex-col divide-y bg-white px-4">
           {isPostLoading && <Loading />}
           {isPostError && <p>Error Data Loading</p>}
           {!isPostLoading &&
             !isPostError &&
-            postList.posts?.length > 0 &&
-            postList.posts.map((item) => (
-              <MarketList key={item.postId} data={item} />
+            (postList.posts?.length > 0 ? (
+              postList.posts.map((item) => (
+                <MarketList key={item.postId} data={item} />
+              ))
+            ) : (
+              <p className="flex flex-1 items-center justify-center text-small text-neutral-disabled">
+                An empty page, waiting for your words!
+              </p>
             ))}
         </div>
         {boardDetail && <WriteButton boardName={boardDetail.boardName} />}
