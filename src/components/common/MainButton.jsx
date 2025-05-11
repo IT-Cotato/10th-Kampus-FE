d4f00dc57a060eeb6cf8a29c65aa8e5d484deb41
seoyeon5117 <seoyun020207@gmail.com>
2025-05-12 02:26:49 +0900

    refactor: MainButton disabled 속성 필수x로 변경

--- src/components/common/MainButton.jsx
+++ src/components/common/MainButton.jsx
@@ -1,15 +1,16 @@
 import { cn } from '@/utils/cn';
 
 export const MainButton = (props) => {
+  const disabled = props.disabled ?? false; // undefined일 경우 false
   return (
     <button
       type="button"
       onClick={props.onClick}
-      disabled={props.disabled}
+      disabled={disabled}
       className={cn(
         'h-[3.6875rem] w-full cursor-pointer rounded-[.625rem] bg-primary-base px-[1.125rem] !text-title-bold-16 text-white',
         {
-          'cursor-default bg-neutral-disabled': props.disabled,
+          'cursor-default bg-neutral-disabled': disabled,
         },
       )}
     >
--- src/components/common/MainWhiteButton.jsx
+++ src/components/common/MainWhiteButton.jsx
@@ -1,16 +1,17 @@
 import { cn } from '@/utils/cn';
 
 export const MainWhiteButton = (props) => {
+  const disabled = props.disabled ?? false; // undefined일 경우 false
   return (
     <button
       type="button"
       onClick={props.onClick}
-      disabled={props.disabled}
+      disabled={disabled}
       className={cn(
         'box-border h-[3.6875rem] w-full cursor-pointer rounded-[.625rem] border border-primary-base bg-white px-[1.125rem] !text-title-bold-16 text-primary-base',
         {
           'cursor-default border-neutral-border-40 text-neutral-border-40':
-            props.disabled,
+            disabled,
         },
       )}
     >
