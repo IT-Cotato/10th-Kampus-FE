ebdb935e87fcafb895899cb39e3c87e4a64d0524
seoyeon5117 <seoyun020207@gmail.com>
2025-05-12 01:32:41 +0900

    design: MainButton 디자인에 따라 수정

--- src/components/common/MainButton.jsx
+++ src/components/common/MainButton.jsx
@@ -7,7 +7,7 @@ export const MainButton = (props) => {
       onClick={props.onClick}
       disabled={props.disabled}
       className={cn(
-        'h-[3.6875rem] w-full rounded-[.625rem] bg-primary-base px-[1.125rem] text-white',
+        'h-[3.6875rem] w-full cursor-pointer rounded-[.625rem] bg-primary-base px-[1.125rem] !text-title-bold-16 text-white',
         {
           'cursor-default bg-neutral-disabled': props.disabled,
         },
