7809b8a4f9ad2c464fd2ba6f0db3b3ba4a788931
seoyeon5117 <seoyun020207@gmail.com>
2025-05-26 00:46:00 +0900

    refactor: api 변경에 따라 write 버튼 refactor

--- src/components/board/write/WriteButton.jsx
+++ src/components/board/write/WriteButton.jsx
@@ -3,21 +3,18 @@ import Pencil from '@/assets/imgs/pencil.svg';
 
 export const WriteButton = ({ boardName = null }) => {
   const navigate = useNavigate();
-  const handleClickWrite = () => {
-    if (boardName) {
-      navigate('./write', { state: { boardName: boardName } });
-    } else {
-      navigate('./write');
-    }
+  const handleClickWrite = (e) => {
+    e.stopPropagation();
+    navigate('./write');
   };
   return (
     <button
       type="button"
-      className="absolute bottom-9 left-1/2 flex -translate-x-1/2 cursor-pointer items-center gap-[.375rem] rounded-full bg-primary-base px-[1.875rem] py-2 text-white shadow-[.1875rem_.1875rem_.25rem_0rem_rgba(0,0,0,0.2)]"
+      className="fixed bottom-9 left-1/2 z-10 flex -translate-x-1/2 cursor-pointer items-center gap-[.375rem] rounded-full bg-primary-base px-[1.875rem] py-2 text-white shadow-[.1875rem_.1875rem_.25rem_0rem_rgba(0,0,0,0.2)]"
       onClick={handleClickWrite}
     >
       <span>Write</span>
-      <img src={Pencil} alt="" className="h-4 w-4" />
+      <img src={Pencil} alt="" className="h-4 w-4 text-white" />
     </button>
   );
 };
