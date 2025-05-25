9eb24a5b018f12482850c6c873f9430dee860d89
seoyeon5117 <seoyun020207@gmail.com>
2025-05-26 00:02:56 +0900

    feat: 포커스 이미지 슬라이더 배경 클릭 이벤트 & 사진 1/6 이상 넘겨야 넘어가도록

--- src/components/common/FocusImageSlider.jsx
+++ src/components/common/FocusImageSlider.jsx
@@ -19,6 +19,8 @@ export const FocusImageSlider = ({
     };
   }, []);
 
+  const [isBackgroundTouched, setIsBackgroundTouched] = useState(false);
+
   const [touch, setTouch] = useState({
     start: 0,
     end: 0,
@@ -42,7 +44,7 @@ export const FocusImageSlider = ({
         result > -flexRef.current.clientWidth * (images.length - 1)
       ) {
         setStyle({
-          transform: `translate3d(${result}px, 0px, 0px)`,
+          transform: `translateX(${result}px)`,
           transition: '0ms',
         });
       }
@@ -51,7 +53,7 @@ export const FocusImageSlider = ({
 
   const touchEnd = (e) => {
     const end = e.changedTouches[0].pageX;
-    if (touch.start > end) {
+    if (touch.start - end > flexRef.current.clientWidth / 6) {
       if (currentImgIndex < images.length - 1) {
         setCurrentImgIndex((prev) => prev + 1);
         setStyle({
@@ -59,7 +61,7 @@ export const FocusImageSlider = ({
           transition: `all 0.4s ease-in-out`,
         });
       }
-    } else {
+    } else if (end - touch.start > flexRef.current.clientWidth / 6) {
       if (currentImgIndex > 0) {
         setCurrentImgIndex((prev) => prev - 1);
         setStyle({
@@ -67,6 +69,11 @@ export const FocusImageSlider = ({
           transition: `all 0.4s ease-in-out`,
         });
       }
+    } else {
+      setStyle({
+        transform: `translateX(-${currentImgIndex}00%)`,
+        transition: `all 0.3s ease-in-out`,
+      });
     }
 
     setTouch({
@@ -75,25 +82,38 @@ export const FocusImageSlider = ({
     });
   };
 
+  // 배경 클릭 시 닫기 버튼 및 사진 인덱스 안보이게
+  const handleTouchBackground = () => {
+    setIsBackgroundTouched((prev) => !prev);
+  };
+
+  const handleClose = (e) => {
+    e.stopPropagation();
+    setImageFocus(false);
+  };
+
   return (
     <ModalPortal>
-      <div className="justsify-center fixed z-[200] mx-auto flex h-full min-h-dvh w-full max-w-lg flex-col justify-center bg-black">
-        <div className="absolute top-5 flex w-full items-center justify-center text-subTitle text-white">
-          <h1>
-            {currentImgIndex + 1}/{images && images.length}
-          </h1>
-          <Close
-            aria-label="Close button"
-            className="absolute left-5 h-5 w-5 text-white"
-            onClick={() => setImageFocus(false)}
-          />
-        </div>
-        <div
-          className="relative"
-          onTouchStart={touchStart}
-          onTouchMove={touchMove}
-          onTouchEnd={touchEnd}
-        >
+      <div
+        className="justsify-center fixed inset-0 z-[200] mx-auto flex h-full min-h-dvh w-full max-w-lg flex-col justify-center bg-black"
+        onTouchStart={touchStart}
+        onTouchMove={touchMove}
+        onTouchEnd={touchEnd}
+        onClick={handleTouchBackground}
+      >
+        {!isBackgroundTouched && (
+          <div className="absolute top-5 flex w-full items-center justify-center text-subTitle text-white">
+            <h1>
+              {currentImgIndex + 1}/{images && images.length}
+            </h1>
+            <Close
+              aria-label="Close button"
+              className="absolute left-5 h-5 w-5 text-white"
+              onClick={handleClose}
+            />
+          </div>
+        )}
+        <div className="relative">
           <div className="max-h-[60vh] w-full overflow-hidden">
             <div ref={flexRef} className="flex" style={style}>
               {images &&
