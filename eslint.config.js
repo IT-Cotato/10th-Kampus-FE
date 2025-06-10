09fccbd7b789586475ad7598715030bfa2af7e7b
“chae-dahee” <cdh010126r@gmail.com>
2025-06-10 16:25:31 +0900

    fix: 린트 flat config 으로 업그레이드

---
+++ eslint.config.js
@@ -0,0 +1,43 @@
+import js from '@eslint/js';
+import globals from 'globals';
+import { defineConfig } from 'eslint/config';
+import pluginReact from 'eslint-plugin-react';
+import pluginReactHooks from 'eslint-plugin-react-hooks';
+import pluginReactRefresh from 'eslint-plugin-react-refresh';
+import unusedImports from 'eslint-plugin-unused-imports';
+import pluginPrettier from 'eslint-plugin-prettier';
+
+export default defineConfig([
+  {
+    files: ['**/*.{js,mjs,cjs,jsx}'],
+    plugins: {
+      js,
+      'unused-imports': unusedImports,
+      react: pluginReact,
+      'react-hooks': pluginReactHooks,
+      'react-refresh': pluginReactRefresh,
+      prettier: pluginPrettier,
+    },
+    extends: ['js/recommended', pluginReact.configs.flat.recommended],
+    languageOptions: { globals: globals.browser },
+    rules: {
+      'react/prop-types': 'off',
+      'react/react-in-jsx-scope': 'off',
+      'react-hooks/rules-of-hooks': 'error',
+      'react-refresh/only-export-components': 'error',
+
+      'no-unused-vars': 'off',
+      'unused-imports/no-unused-imports': 'warn',
+      'unused-imports/no-unused-vars': [
+        'warn',
+        {
+          vars: 'all',
+          varsIgnorePattern: '^_',
+          args: 'after-used',
+          argsIgnorePattern: '^_',
+        },
+      ],
+      'prettier/prettier': 'warn',
+    },
+  },
+]);
