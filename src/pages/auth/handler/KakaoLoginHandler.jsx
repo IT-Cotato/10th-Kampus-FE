2a182395b919f9ea39672bc4456c825be808de77
Ki-Tak <lee08tain@gmail.com>
2025-06-16 17:16:33 +0900

    feat: 카카오 로그인 이후 카카오 핸들러 페이지 라우팅 추가

--- src/pages/auth/handler/KakaoLoginHandler.jsx
+++ src/pages/auth/handler/KakaoLoginHandler.jsx
@@ -3,29 +3,46 @@ import { useLocation, useNavigate } from 'react-router-dom';
 import { useAuthStore } from '@/stores/useAuthStore';
 import { Loading } from '@/components/common/Loading';
 import { path } from '@/routes/path';
+import { getUserDetail } from '@/apis/auth/login.api';
 
 export const KakaoLoginHandler = () => {
   const location = useLocation();
   const navigate = useNavigate();
   const { setAccessToken } = useAuthStore();
 
-  /*useEffect(() => {
+  useEffect(() => {
+    // URL에 토큰 정보가 남지 않도록 replace: true 추가
+    const handleLogin = async () => {
+      const { data, success } = await getUserDetail();
+      if (data === undefined) {
+        alert('Login Failed!');
+        navigate(path.login.base, { replace: true });
+        return;
+      }
+      if (success && data.needSetup) {
+        navigate(path.signup.base, { replace: true });
+      } else if (success && !data.needSetup) {
+        navigate(path.home, { replace: true });
+      } else {
+        alert('Login Failed!');
+        navigate(path.login.base, { replace: true });
+      }
+    };
+
     // URL의 쿼리 문자열에서 accessToken을 파싱
     const searchParams = new URLSearchParams(location.search);
     const accessToken = searchParams.get('accessToken');
 
     if (accessToken) {
       setAccessToken(accessToken);
-
-      // URL에 토큰 정보가 남지 않도록 replace: true 추가
-      navigate(path.home, { replace: true });
+      handleLogin();
     } else {
       // 토큰이 없는 경우, alert 창 이후 로그인 창으로 리다이렉트
       console.error('카카오 로그인 처리 중 에러: Access Token이 없습니다.');
-      alert('카카오 로그인에 실패했습니다. 다시 시도해 주세요.');
+      alert('Kakao Login Failed!');
       navigate(path.login.base, { replace: true });
     }
-  }, [location, navigate, setAccessToken]);*/
+  }, [location]);
 
   return (
     <div className="flex h-screen w-screen items-center justify-center">
