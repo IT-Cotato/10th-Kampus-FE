6146e4c9d6761e714a722f5f90d92672bcdf78a6
seoyeon5117 <seoyun020207@gmail.com>
2025-08-11 14:38:19 +0900

    refactor: 보드 디자인 리뉴얼 및 리팩토링

--- src/components/board/BoardList.jsx
+++ src/components/board/BoardList.jsx
@@ -4,22 +4,22 @@ import { cn } from '@/utils/cn';
 export const BoardList = ({ data, listKey, index, togglePin }) => {
   const navigate = useNavigate();
   return (
-    <div className="flex items-start gap-4 px-4 py-4">
+    <div className="flex w-full items-start gap-4 p-4">
       <Pin
-        className={cn('h-6 w-6 cursor-pointer', {
+        className={cn('h-6 w-6 shrink-0 cursor-pointer', {
           'text-primary-base': data.pin,
-          'text-primary-20': !data.pin,
+          'text-primary-10': !data.pin,
         })}
         onClick={() => togglePin(listKey, index, data.order)}
       />
-      <div className="flex h-fit w-full flex-col gap-2 leading-tight">
+      <div className="flex h-fit w-full flex-col gap-1 leading-tight">
         <p
-          className="line-clamp-1 w-full cursor-pointer text-base text-neutral-80"
+          className="text-neutral-90 line-clamp-1 w-full cursor-pointer text-base"
           onClick={() => navigate(`${data.order}`)}
         >
           {data.title}
         </p>
-        <p className="line-clamp-1 w-full text-small text-neutral-border-50">
+        <p className="line-clamp-1 text-small text-neutral-border-50">
           {data.description}
         </p>
       </div>
