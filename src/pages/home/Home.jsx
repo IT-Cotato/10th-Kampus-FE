e0abdcb61ba38507819f368f5a6d83fed7d37652
seoyeon5117 <seoyun020207@gmail.com>
2025-08-12 23:57:15 +0900

    feat: 홈화면 헤더 고정

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,5 +1,5 @@
 import Logo from '@/assets/imgs/icon/kampus-logo.svg?react';
-import search from '@/assets/imgs/icon/search.svg';
+import SearchIcon from '@/assets/imgs/icon/search.svg?react';
 import { useNavigate } from 'react-router-dom';
 import { PATH } from '@/routes/path';
 import { BoardBox } from '@/components/home/BoardBox.jsx';
@@ -32,48 +32,49 @@ export const Home = () => {
   const { data: userDetail, isLoading: _userLoading } = useGetUserData();
 
   return (
-    <div className="flex w-full flex-col gap-6 bg-[#FCFCFC] px-4 py-3">
-      <div className="flex items-start justify-between pb-[.625rem]">
+    <>
+      <header className="fixed top-0 flex h-fit w-full justify-between bg-[#FCFCFC] px-4 py-4 width-fixed">
         <Logo
           aria-label="Kampus Logo"
           className="h-auto w-[6rem] text-primary-base"
         />
-        <button
-          type="button"
-          className="cursor-pointer"
+        <SearchIcon
+          role="button"
+          aria-label="search button"
+          className="h-6 w-6 cursor-pointer"
           onClick={() => navigate(PATH.SEARCH)}
-        >
-          <img src={search} alt="search button" className="h-6 w-6" />
-        </button>
+        />
+      </header>
+      <div className="flex w-full flex-col gap-6 bg-[#FCFCFC] px-4 pb-3 pt-20">
+        {userDetail?.universityId !== -1 && (
+          <h1 className="text-pageTitle text-neutral-title">
+            {userDetail?.universityCode}
+          </h1>
+        )}
+        <Calendar />
+        {userDetail?.universityId !== -1 && !universityError && (
+          <BoardBox
+            data={univeristyList}
+            boardTitle="My univ"
+            university={true}
+            boardId={univeristyList?.boardId}
+          />
+        )}
+        {favoriteList?.homePostThumbnailList.length > 0 && (
+          <BoardBox
+            data={favoriteList?.homePostThumbnailList}
+            boardTitle="Favorites"
+          />
+        )}
+        {trendingList?.homePostThumbnailList.length > 0 && (
+          <BoardBox
+            data={trendingList?.homePostThumbnailList}
+            boardTitle="Trending"
+            boardId={trendingList.homePostThumbnailList.boardId}
+          />
+        )}
+        {/* <CardPostBox data={cardNewsList?.posts} /> */}
       </div>
-      {userDetail?.universityId !== -1 && (
-        <h1 className="text-pageTitle text-neutral-title">
-          {userDetail?.universityCode}
-        </h1>
-      )}
-      <Calendar />
-      {userDetail?.universityId !== -1 && !universityError && (
-        <BoardBox
-          data={univeristyList}
-          boardTitle="My univ"
-          university={true}
-          boardId={univeristyList?.boardId}
-        />
-      )}
-      {favoriteList?.homePostThumbnailList.length > 0 && (
-        <BoardBox
-          data={favoriteList?.homePostThumbnailList}
-          boardTitle="Favorites"
-        />
-      )}
-      {trendingList?.homePostThumbnailList.length > 0 && (
-        <BoardBox
-          data={trendingList?.homePostThumbnailList}
-          boardTitle="Trending"
-          boardId={trendingList.homePostThumbnailList.boardId}
-        />
-      )}
-      {/* <CardPostBox data={cardNewsList?.posts} /> */}
-    </div>
+    </>
   );
 };
