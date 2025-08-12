26c3d315308dbc5dcb67d476a7935fab63673f88
“chae-dahee” <cdh010126r@gmail.com>
2025-08-12 16:17:22 +0900

    fix: 소문자 git 추적 해재
    컴포넌트, 페이지 파일에서 소문자와 대문자 모두 추적하는 문제 발생했습니다.
    git index 에서 소문자 파일을 제거했습니다.

--- src/components/chat/newMsgCnt.jsx
+++
@@ -1,7 +0,0 @@
-export const NewMsgCnt = ({ cnt }) => {
-  return (
-    <div className="h-[1.25rem] rounded-[6.25rem] bg-primary-red px-2 text-small text-white">
-      {cnt}
-    </div>
-  );
-};
--- src/components/chat/reportModal.jsx
+++
@@ -1,7 +0,0 @@
-import { Modal } from '@/components/common/Modal';
-
-export const reportModal = ({ isOpen, onClose }) => {
-  if (!isOpen) return null;
-
-  return <Modal title="Successfully report" onClose={onClose}></Modal>;
-};
