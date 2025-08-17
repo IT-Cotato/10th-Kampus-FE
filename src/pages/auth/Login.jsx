038e8b9a50eb15547014e9703b09471151030f73
seoyeon5117 <seoyun020207@gmail.com>
2025-08-17 20:08:10 +0900

    feat: 로그인 페이지에 Contact Us 메일 연결

--- src/pages/auth/Login.jsx
+++ src/pages/auth/Login.jsx
@@ -1,23 +1,19 @@
 import Logo from '@/assets/imgs/icon/kampus-logo.svg?react';
 import { KakaoLogin } from '@/components/common/KakaoLogin';
 import { GoogleLogin } from '@/components/common/GoogleLogin';
-import { useNavigate } from 'react-router-dom';
-import { PATH } from '@/routes/path';
 import { Bubble } from '@/components/common/Bubble';
+import OfficialMail from '@/constants/officialMail.json';
 
 export const Login = () => {
-  const navigate = useNavigate();
-
   return (
     <div className="relative flex flex-1 items-center">
-      <button
-        onClick={() =>
-          navigate(`${PATH.MYPAGE.BASE}/${PATH.MYPAGE.SERVICE.BASE}`)
-        }
+      <a
+        aria-label="Contact us button"
         className="absolute right-4 top-4 px-4 py-2 text-base text-neutral-base"
+        href={`mailto:${OfficialMail.mail}`}
       >
         Contact us
-      </button>
+      </a>
       <div className="flex w-full -translate-y-12 flex-col items-center justify-center gap-6 px-8">
         <Logo className="w-40 text-primary-base" />
         <div className="flex w-full flex-col gap-4">
