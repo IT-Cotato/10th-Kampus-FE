a484572c339b6aac2890f818fbc994e40f5fe0b2
seoyeon5117 <seoyun020207@gmail.com>
2025-08-17 19:44:37 +0900

    feat: 마이페이지 내 게시글, 댓글, 스크랩 조회

--- src/pages/my/community/CommunityScrap.jsx
+++ src/pages/my/community/CommunityScrap.jsx
@@ -22,7 +22,7 @@ export const CommunityScrap = () => {
     <div className="flex h-full w-full flex-col">
       {isLoading ? (
         <Loading />
-      ) : !postList.post ? (
+      ) : !postList.items ? (
         <div className="flex h-full w-full -translate-y-10 flex-col items-center justify-center gap-2">
           <Logo className="w-32 text-neutral-disabled" />
           <span className="text-center text-neutral-border-40">
@@ -33,7 +33,7 @@ export const CommunityScrap = () => {
         </div>
       ) : (
         <div className="flex w-full flex-1 flex-col divide-y bg-white">
-          {postList.posts.map((item, index) => (
+          {postList.items.map((item, index) => (
             <PostList
               key={index}
               data={item}
