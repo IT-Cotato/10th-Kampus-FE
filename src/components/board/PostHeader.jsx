f63d14393dcd748e0ba7c57025fc05b846e1df3a
seoyeon5117 <seoyun020207@gmail.com>
2025-08-31 22:39:57 +0900

    fix: 게시판 헤더 메뉴바 안뜨는 오류 해결

--- src/components/board/PostHeader.jsx
+++ src/components/board/PostHeader.jsx
@@ -136,7 +136,8 @@ export const PostHeader = ({ isAuthor = false }) => {
             <Search className="h-6 w-6 cursor-pointer text-neutral-title" />
           </button>
         )}
-        {(boardDetail?.boardType === BOARD_TYPE.NORMAL ||
+        {(boardDetail?.boardWithFavoriteStatus.boardType ===
+          BOARD_TYPE.NORMAL ||
           postId ||
           productId) && (
           <BoardMenuBar
