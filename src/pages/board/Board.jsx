d05aa15009417fa404ca2e49d72b227b95cec907
seoyeon5117 <seoyun020207@gmail.com>
2025-08-31 23:34:27 +0900

    feat: market, board 게시글 없는 케이스 추가

--- src/pages/board/Board.jsx
+++ src/pages/board/Board.jsx
@@ -1,3 +1,4 @@
+import Logo from '@/assets/imgs/icon/kampus-logo.svg?react';
 import { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import { PostList } from '@/components/board/PostList';
@@ -64,7 +65,7 @@ export const Board = () => {
   return (
     <div className="flex flex-1">
       <PostHeader />
-      <div className="flex h-fit w-full flex-col pt-14">
+      <div className="flex w-full flex-1 flex-col pt-14">
         <div className="fixed z-10 flex w-full gap-[0.875rem] bg-white px-[1.125rem] pb-4 pt-[.875rem] width-fixed">
           {boardDetail?.boardWithFavoriteStatus?.usesCategories && (
             <FilterBox
@@ -81,6 +82,25 @@ export const Board = () => {
             selected={sortOrder}
           />
         </div>
+        {posts.length === 0 &&
+          (boardDetail?.boardWithFavoriteStatus?.boardType ===
+          BOARD_TYPE.CARD ? (
+            <div className="flex h-full w-full -translate-y-10 flex-col items-center justify-center gap-2">
+              <Logo className="w-32 text-neutral-disabled" />
+              <span className="text-center text-neutral-border-40">
+                Coming Soon!
+              </span>
+            </div>
+          ) : (
+            <div className="flex h-full w-full -translate-y-10 flex-col items-center justify-center gap-2">
+              <Logo className="w-32 text-neutral-disabled" />
+              <span className="text-center text-neutral-border-40">
+                There are no posts yet.
+                <br />
+                Please write the first post!
+              </span>
+            </div>
+          ))}
         <div className="flex w-full flex-1 flex-col divide-y overflow-y-auto bg-white px-4 pt-[3.25rem]">
           {/* 카드 뉴스 리스트 뷰, 일반 게시판 리스트 뷰의 UI가 다름 */}
           {posts.map((item, index) =>
--- src/pages/market/Market.jsx
+++ src/pages/market/Market.jsx
@@ -1,3 +1,4 @@
+import Logo from '@/assets/imgs/icon/kampus-logo.svg?react';
 import { FilterBox } from '@/components/board/FilterBox';
 import { PostHeader } from '@/components/board/PostHeader';
 import { WriteButton } from '@/components/board/write/WriteButton';
@@ -33,16 +34,13 @@ export const Market = () => {
   }, [sortOrder]);
 
   const { data: categoryData } = useGetMarketCategory();
-  const {
-    data: productList,
-    isPending: isPostLoading,
-    isError: isPostError,
-  } = useGetMarketProductList({ pageParam: 1, sortKey, category });
+  const { data: productList, isPending: isPostLoading } =
+    useGetMarketProductList({ pageParam: 1, sortKey, category });
 
   return (
     <div className="flex flex-1">
       <PostHeader />
-      <div className="flex h-fit w-full flex-col pt-14">
+      <div className="flex h-full w-full flex-1 flex-col pt-14">
         <div className="fixed z-10 flex w-full max-w-lg gap-[0.875rem] bg-white px-[1.125rem] pb-4 pt-[.875rem]">
           {categoryData && (
             <FilterBox
@@ -59,21 +57,27 @@ export const Market = () => {
             selected={sortOrder}
           />
         </div>
-        <div className="flex w-full flex-1 flex-col divide-y overflow-y-auto bg-white px-4 pt-[3.25rem]">
-          {isPostLoading && <Loading />}
-          {isPostError && <p>An error occured while loading</p>}
-          {!isPostLoading &&
-            !isPostError &&
-            (productList?.items?.length > 0 ? (
+        {isPostLoading && (
+          <div className="flex h-full w-full flex-1 items-center justify-center">
+            <Loading />
+          </div>
+        )}
+        {!isPostLoading && productList?.items.length === 0 && (
+          <div className="flex h-full w-full -translate-y-10 flex-col items-center justify-center gap-2">
+            <Logo className="w-32 text-neutral-disabled" />
+            <span className="text-center text-neutral-border-40">
+              Be the first to trade your stuffs!
+            </span>
+          </div>
+        )}
+        {!isPostLoading && (
+          <div className="flex w-full flex-1 flex-col divide-y overflow-y-auto bg-white px-4 pt-[3.25rem]">
+            {productList?.items?.length > 0 &&
               productList?.items.map((item) => (
                 <MarketList key={item.productId} data={item} />
-              ))
-            ) : (
-              <p className="flex flex-1 items-center justify-center text-small text-neutral-disabled">
-                An empty page, waiting for your words!
-              </p>
-            ))}
-        </div>
+              ))}
+          </div>
+        )}
         <WriteButton />
       </div>
     </div>
