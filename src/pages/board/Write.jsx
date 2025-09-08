2a42f67e8f0a4158b07b74306b9ee15d9aaa3aab
seoyeon5117 <seoyun020207@gmail.com>
2025-09-08 20:53:23 +0900

    chore: useGetPost, useGetBoardPost 같은 api라서 하나로 통일

--- src/pages/board/Write.jsx
+++ src/pages/board/Write.jsx
@@ -7,7 +7,7 @@ import { useEffect, useRef, useState } from 'react';
 import { useNavigate, useParams, useSearchParams } from 'react-router-dom';
 import { SelectCategory } from '@/components/board/write/SelectCategory';
 import { TranslateModal } from '@/components/common/TranslateModal';
-import { useGetBoardPost } from '@/state/query/board/useGetBoardPost';
+import { useGetBoardPost } from '@/state/query/post/useGetPost';
 import { urlToFile } from '@/utils/urlToFile';
 import { usePutBoardPost } from '@/state/mutation/board/usePutBoardPost';
 import { usePostWriteTranslate } from '@/state/mutation/common/usePostWriteTranslate';
--- src/state/query/post/useGetPost.js
+++ src/state/query/post/useGetPost.js
@@ -1,15 +1,16 @@
+import { getPostDetail } from '@/apis/board/handlePost.api';
 import { QUERY_KEYS } from '@/constants/api';
 import { useQuery } from '@tanstack/react-query';
-import { getPostDetail } from '@/apis/board/handlePost.api';
 import { useParams } from 'react-router-dom';
 
 export const useGetPost = () => {
   const { postId } = useParams();
+
   const query = useQuery({
-    queryFn: () => getPostDetail({ postId: postId }),
+    queryFn: () => getPostDetail({ postId }),
     queryKey: [QUERY_KEYS.GET_POST_DETAIL, postId],
     select: (res) => res.postDetails,
-    staleTime: 20 * 1000,
+    enabled: !!postId,
   });
   return query;
 };
